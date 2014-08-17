8bbe28414dec86be6ed7c92a162bd0dd744100b7
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-17 13:49:18 +0300

    My methods !
    Add method SplitStringToIntArray

---
+++ MyMethods/SplitStringToIntArray/SplitStringToIntArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class SplitStringToIntArray
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int[] intArray = SplitStringsToInts(input);
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            Console.WriteLine(intArray[i]);
+        }
+    }
+
+    private static int[] SplitStringsToInts(string s)
+    {
+        string[] strArray = s.Split(' ');
+        int[] intArray = new int[strArray.Length];
+
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            intArray[i] = int.Parse(strArray[i]);
+        }
+        return intArray;
+    }
+}
