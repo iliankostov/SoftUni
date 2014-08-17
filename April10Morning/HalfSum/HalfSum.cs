3ed34e62cb6cc5f8990627c3cac18c259c25a2f8
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-17 14:36:27 +0300

    April10Morning
    HalfSum at 100 pts.

---
+++ April10Morning/HalfSum/HalfSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class HalfSum
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int sumOne = 0;
+        int sumTwo = 0;
+        for (int i = 1; i <= number; i++)
+        {
+            sumOne += int.Parse(Console.ReadLine());
+        }
+        for (int i = 1; i <= number; i++)
+        {
+            sumTwo += int.Parse(Console.ReadLine());
+        }
+        if (sumOne == sumTwo)
+        {
+            Console.WriteLine("Yes, sum={0}", sumOne);
+        }
+        else
+        {
+            Console.WriteLine("No, diff={0}", Math.Abs(sumOne - sumTwo));
+        }
+    }
+}
