78f1f0b771c95ea4e65e2ba3b809cf6df3c7858d
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-17 14:16:42 +0300

    April10Morning
    JoroTheFootballPlayer at 100 pts.

---
+++ April10Morning/JoroTheFootballPlayer/JoroTheFootballPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class JoroTheFootballPlayer
+{
+    static void Main()
+    {
+        string isYearLeap = Console.ReadLine();
+        int holidays = int.Parse(Console.ReadLine());
+        int hometowm = int.Parse(Console.ReadLine());
+        double plays = 0;
+
+        plays = hometowm + (52 - hometowm) * 2.0 / 3 + holidays * 1.0 / 2;
+        if (isYearLeap == "t")
+        {
+            plays += 3;
+        }
+        Console.WriteLine((int)plays);
+    }
+}
