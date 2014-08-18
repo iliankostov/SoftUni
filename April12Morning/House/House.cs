adc4cdbdf9fa94868e5e125d644c8c382ef3b89f
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-18 18:15:15 +0300

    C# Practice

---
+++ April12Morning/House/House.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class House
+{
+    static void Main()
+    {
+        int width = int.Parse(Console.ReadLine());
+        char asterisk = '*';
+        char dot = '.';
+        int outDots = (width - 1) / 2;
+        int inDots = 1;
+        int botDots = (int)(width / 4);
+
+        for (int i = 1; i <= width; i++)
+        {
+            if (i == 1)
+            {
+                Console.WriteLine(new String(dot, outDots) + asterisk + new String(dot, outDots));
+                outDots--;
+            }
+            else if (i < (width + 1) / 2)
+            {
+                Console.WriteLine(new String(dot, outDots) + asterisk + new String(dot, inDots) + asterisk + new String(dot, outDots));
+                outDots--;
+                inDots += 2;
+            }
+            else if (i == (width + 1) / 2)
+            {
+                Console.WriteLine(new String(asterisk, width));
+            }
+            else if (i > (width + 1) / 2 && i < width)
+            {
+                Console.WriteLine(new String(dot, botDots) + asterisk + new String(dot, width - (botDots + 1) * 2) + asterisk + new String(dot, botDots));
+            }
+            else
+            {
+                Console.WriteLine(new String(dot, botDots) + new String(asterisk, width - botDots * 2) + new String(dot, botDots));
+            }
+        }
+    }
+}
---
+++ April12Morning/Triangle/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Triangle
+{
+    static void Main()
+    {
+        int aX = int.Parse(Console.ReadLine());
+        int aY = int.Parse(Console.ReadLine());
+        int bX = int.Parse(Console.ReadLine());
+        int bY = int.Parse(Console.ReadLine());
+        int cX = int.Parse(Console.ReadLine());
+        int cY = int.Parse(Console.ReadLine());
+
+        double aSide = Math.Sqrt(Math.Pow((bX - aX), 2) + Math.Pow((bY - aY), 2));
+        double bSide = Math.Sqrt(Math.Pow((cX - bX), 2) + Math.Pow((cY - bY), 2));
+        double cSide = Math.Sqrt(Math.Pow((aX - cX), 2) + Math.Pow((aY - cY), 2));
+
+        if (aSide + bSide > cSide && bSide + cSide > aSide && aSide + cSide > bSide)
+        {
+            double perimeter = (aSide + bSide + cSide) / 2;
+            double area = Math.Sqrt(perimeter*(perimeter - aSide)*(perimeter - bSide)*(perimeter - cSide));
+            Console.WriteLine("Yes\n{0:0.00}", area);
+        }
+        else
+        {
+            Console.WriteLine("No\n{0:0.00}", aSide);
+        }      
+    }
+}
