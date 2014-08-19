805ba27db22bc14f5e47182b7cfd43fdb6b03988
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-19 17:14:41 +0300

    C# Practice

---
+++ April12Evening/ExamSchedule/ExamSchedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ExamSchedule
+{
+    static void Main()
+    {
+        string beginHour = Console.ReadLine();
+        string beginMinute = Console.ReadLine();
+        string partOfDay = Console.ReadLine();
+        int examHour = int.Parse(Console.ReadLine());
+        int examMinute = int.Parse(Console.ReadLine());
+
+        DateTime begin = DateTime.Parse(beginHour + ":" + beginMinute + " " + partOfDay);
+        TimeSpan exam = TimeSpan.Parse(examHour + ":" + examMinute);
+        DateTime end = begin + exam;
+
+        Console.WriteLine("{0:hh:mm:tt}", end);    
+    }
+}
