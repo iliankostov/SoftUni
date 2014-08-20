c567f36e9fc5b96c5dc1d81ce838aae7e9d6a73e
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-20 12:46:20 +0300

    C# Practice

---
+++ April11Evening/NewHouse/NewHouse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class NewHouse
+{
+    static void Main()
+    {
+        int height = int.Parse(Console.ReadLine());
+        char asterisk = '*';
+        char underline = '-';
+        char wall = '|';
+
+        int roofHeight = (height + 1) / 2;
+        int outRoof = (height - 1) / 2;
+        int roof = 1;
+
+        for (int i = 1; i <=  height + roofHeight; i++)
+        {
+            if (i <= roofHeight)
+            {
+                Console.WriteLine(new String(underline, outRoof) + new String(asterisk, roof) + new String(underline, outRoof));
+                outRoof--;
+                roof += 2;
+            }
+            else
+            {
+                Console.WriteLine(new String(wall, 1) + new String(asterisk, height - 2) + new String(wall, 1));
+            }
+        }
+    }
+}
