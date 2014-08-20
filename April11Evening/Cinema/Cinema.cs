1ec68c8f1124d3f391db23babc0b38cad323664c
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-20 12:33:09 +0300

    C# Practice

---
+++ April11Evening/Cinema/Cinema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Cinema
+{
+    static void Main()
+    {
+        string typeProjection = Console.ReadLine();
+        int rows = int.Parse(Console.ReadLine());
+        int columns = int.Parse(Console.ReadLine());
+        int persons = rows * columns;
+        decimal income = 0m;
+
+        if (typeProjection == "Premiere")
+        {
+            income = 12.00m * persons;
+        }
+        else if (typeProjection == "Normal")
+        {
+            income = 7.50m * persons;
+        }
+        else
+        {
+            income = 5.00m * persons;
+        }
+        Console.WriteLine("{0} leva", income);
+    }
+}
---
+++ April11Evening/Illuminati/Illuminati.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Illuminati
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int numberOfVowels = 0;
+        int sum = 0;
+        
+        foreach (char charToCheck in input)
+        {
+            switch (charToCheck)
+            {
+                case 'A':
+                    numberOfVowels++;
+                    sum+=65;
+                    break;
+                case 'a':
+                    numberOfVowels++;
+                    sum+=65;
+                    break;
+                case 'E':
+                    numberOfVowels++;
+                    sum+=69;
+                    break;
+                case 'e':
+                    numberOfVowels++;
+                    sum+=69;
+                    break;
+                case 'I':
+                    numberOfVowels++;
+                    sum+=73;
+                    break;
+                case 'i':
+                    numberOfVowels++;
+                    sum+=73;
+                    break;
+                case 'O':
+                    numberOfVowels++;
+                    sum+=79;
+                    break;
+                case 'o':
+                    numberOfVowels++;
+                    sum+=79;
+                    break;
+                case 'U':
+                    numberOfVowels++;
+                    sum+=85;
+                    break;
+                case 'u':
+                    numberOfVowels++;
+                    sum+=85;
+                    break;
+                default:
+                    break;
+            }            
+        }
+
+        Console.WriteLine(numberOfVowels);
+        Console.WriteLine(sum);
+        
+    }
+}
