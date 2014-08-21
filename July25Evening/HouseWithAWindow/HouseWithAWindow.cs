283df5fe0a44af616ca9bc3b10442d5ae699ac3c
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-21 20:50:00 +0300

    C# Practice

---
+++ July25Evening/HouseWithAWindow/HouseWithAWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class HouseWithAWindow
+{
+    static void Main()
+    {
+        int height = int.Parse(Console.ReadLine());
+        char dots = '.';
+        char asterisk = '*';
+
+        int inDots = 1;
+        int outDots = (height - 1);
+        int windowHeight = height / 2;
+        int windowWidth = height - 3;
+
+        for (int i = 1; i <= height; i++)
+        {
+            if (i == 1)
+	        {
+		        Console.WriteLine(new String(dots, outDots) + asterisk + new String(dots, outDots));
+                outDots--;
+	        }
+            else
+	        {
+                Console.WriteLine(new String(dots, outDots) + asterisk + new String(dots, inDots) + asterisk + new String(dots, outDots));
+                outDots--;
+                inDots += 2;
+	        }            
+        }
+        Console.WriteLine(new String(asterisk, 2*height - 1));
+        for (int i = 1; i <= height; i++)
+        {
+            if (i >= 1 && i <= height / 4)
+            {
+                Console.WriteLine(asterisk + new String(dots, 2 * height - 3) + asterisk);
+            }
+            else if (i > height / 4 && i <= (height / 2 + height / 4))
+            {
+                Console.WriteLine(asterisk + new String(dots, (height - 1 - (windowWidth + 1) /2)) +
+                                            new String(asterisk, windowWidth) + 
+                                            new String(dots, (height- 1 - (windowWidth + 1) / 2)) + asterisk);
+            }
+            else
+            {
+                Console.WriteLine(asterisk + new String(dots, 2 * height - 3) + asterisk);
+            }
+        }
+        Console.WriteLine(new String(asterisk, 2 * height - 1));
+    }
+}
---
+++ July25Evening/OddAndEvenJumps/OddAndEvenJumps.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class OddAndEvenJumps
+{
+    static void Main()
+    {
+        string input = Console.ReadLine().ToLower().Replace(" ", "");
+        int oddJump = int.Parse(Console.ReadLine());
+        int evenJump = int.Parse(Console.ReadLine());
+
+        int oddCounter = 0;
+        ulong oddResult = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                oddCounter++;
+                if (oddCounter % oddJump == 0)
+                {
+                    oddResult *= input[i];
+                }
+                else
+                {
+                    oddResult += input[i];
+                }
+            }
+        }
+        string oddSum = oddResult.ToString("X");
+        Console.WriteLine("Odd: " + oddSum);
+
+        int evenCounter = 0;
+        ulong evenResult = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i % 2 != 0)
+            {
+                evenCounter++;
+                if (evenCounter % evenJump == 0)
+                {
+                    evenResult *= input[i];
+                }
+                else
+                {
+                    evenResult += input[i];
+                }
+            }
+        }
+        string evenSum = evenResult.ToString("X");
+        Console.WriteLine("Even: " + evenSum);
+
+      
+    }   
+}
