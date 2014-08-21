8564a824e47234301c5805cdf0897d8c5b39a5ce
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-21 21:30:32 +0300

    My C# Methods

---
+++ MyMethods/GetNextLetter/GetNextLetter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class GetNextLetter
+{
+    static void Main()
+    {
+        char letter = char.Parse(Console.ReadLine());
+        for (int i = 0; i < 30; i++)
+        {           
+            letter = GetNextLetters(letter);
+            Console.WriteLine(letter);
+        }
+    }
+
+    static char GetNextLetters(char letter)
+    {
+        letter++;
+        if (letter > 'Z')
+        {
+            letter = 'A';
+        }
+        return letter;
+    }
+}
