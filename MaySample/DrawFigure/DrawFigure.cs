c2ea4e5023e58e73cd90dd6203bd9647feade494
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-21 11:03:38 +0300

    C# Practice

---
+++ MaySample/DrawFigure/DrawFigure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class DrawFigure
+{
+    static void Main()
+    {
+        int heigth = int.Parse(Console.ReadLine());
+
+        int dots = 0;
+        int asterisk = heigth;
+
+        string topDownLine = new String('*', heigth);
+        string middleLane = new String('.', (heigth -1) / 2) + '*' + new String('.', (heigth -1) / 2);
+
+        for (int i = 1; i <= heigth; i++)
+        {
+            if (i == 1)
+            {
+                Console.WriteLine(topDownLine);
+                
+            }
+            else if (i < (heigth + 1) / 2)
+            {
+                asterisk -= 2;
+                dots++;
+                Console.WriteLine(new String('.', dots) + new String('*', asterisk) + new String('.', dots));              
+            }
+            else if (i == (heigth + 1) / 2)
+            {
+                Console.WriteLine(middleLane);
+            }
+            else if (i > (heigth + 1) / 2 && i < heigth)
+            {
+                Console.WriteLine(new String('.', dots) + new String('*', asterisk) + new String('.', dots));
+                dots--;
+                asterisk += 2;
+            }
+            else
+            {
+                Console.WriteLine(topDownLine);
+            }                      
+        }
+    }
+}
