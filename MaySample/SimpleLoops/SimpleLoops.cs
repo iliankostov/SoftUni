ef341c0b7efbc40878299fd8ea80781936d2baeb
Ilian Kostov <ilian.kostov@gmail.com>
2014-08-21 10:47:27 +0300

    C# Practice

--- MaySample/SimpleLoops/SimpleLoops.cs
+++ MaySample/SimpleLoops/SimpleLoops.cs
@@ -1,9 +1,41 @@
 ﻿using System;
+using System.Numerics;
 
 class SimpleLoops
 {
     static void Main()
-    {
+    {      
+        BigInteger t1 = BigInteger.Parse(Console.ReadLine());
+        BigInteger t2 = BigInteger.Parse(Console.ReadLine());
+        BigInteger t3 = BigInteger.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
 
+        BigInteger tN = t3 + t2 + t1;
+        if (n < 4)
+        {
+            if (n == 1)
+            {
+                tN = t1;
+            }
+            else if (n == 2)
+            {
+                tN = t2;
+            }
+            else
+            {
+                tN = t3;
+            }
+        }
+        else
+        {
+            for (int i = 4; i < n; i++)
+            {
+                t1 = t2;
+                t2 = t3;
+                t3 = tN;
+                tN = t3 + t2 + t1;
+            }
+        }
+        Console.WriteLine(tN);
     }
 }
