b110a051c51a842f7d3d3a4ae0a946cd9296f916
ilian <ilian.kostov@gmail.com>
2014-09-06 23:06:24 +0300

    Java Homework

---
+++ 01-Intro-Java-Homework/src/P05CurrentDateTime.java
@@ -0,0 +1,13 @@
+
+import java.util.Date;
+
+public class P05CurrentDateTime {
+
+	public static void main(String[] args) {
+		
+		Date CurentDateTime = new Date();
+		System.out.println(CurentDateTime);		
+		
+	}
+	
+}
