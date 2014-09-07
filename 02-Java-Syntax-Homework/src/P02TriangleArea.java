657762e3603be12e4bae358019c527ff59317039
ilian <ilian.kostov@gmail.com>
2014-09-07 19:48:55 +0300

    Java Homework

---
+++ 02-Java-Syntax-Homework/src/P02TriangleArea.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class P02TriangleArea {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int ax, ay, bx, by, cx, cy;
+		double a, b, c, perimeter, area;		
+		ax = scan.nextInt();
+		ay = scan.nextInt();
+		bx = scan.nextInt();
+		by = scan.nextInt();
+		cx = scan.nextInt();
+		cy = scan.nextInt();
+		a = Math.sqrt(Math.pow((bx - ax), 2) + Math.pow((by - ay), 2));
+		b = Math.sqrt(Math.pow((cx - bx), 2) + Math.pow((cy - by), 2));
+		c = Math.sqrt(Math.pow((ax - cx), 2) + Math.pow((ay - cy), 2));
+		
+		if ((a + b > c) && (b + c > a) && (c + a > b)) {
+			perimeter = (a + b + c)/2;
+			area = Math.sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c));
+			System.out.println(Math.round(area));
+		}
+		else {
+			System.out.println(0);
+		}
+		
+	}
+}
