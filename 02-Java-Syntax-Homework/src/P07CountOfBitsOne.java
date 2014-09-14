98253f18eb49e4d7716063e0fafbc8b7acf8690b
ilian <ilian.kostov@gmail.com>
2014-09-14 12:18:07 +0300

    Java Homework

---
+++ 02-Java-Syntax-Homework/src/P07CountOfBitsOne.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class P07CountOfBitsOne {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		try {
+			int number = scanner.nextInt();
+			scanner.close();
+			int output = numOnesInBinary(number);
+			System.out.println(output);
+		} catch (NumberFormatException nfex) {
+			System.err.println("Invalid input " + nfex);
+		}
+							
+	}
+	
+	public static int numOnesInBinary(int n) {
+
+		  if (n < 0) return -1;
+
+		  int j = 0;
+		  while ( n > Math.pow(2, j)) j++;
+
+		  int result = 0;
+		  for (int i=j; i >=0; i--){
+		    if (n >= Math.pow(2, i)) {
+		        n = (int) (n - Math.pow(2,i));
+		        result++;    
+		    }
+		  }
+
+		  return result;
+		}
+}
