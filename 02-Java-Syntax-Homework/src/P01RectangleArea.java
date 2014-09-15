d2b066377cf55d630cd1da4ee28557b6d7f23c4f
ilian <ilian.kostov@gmail.com>
2014-09-15 21:05:33 +0300

    Java Homework

--- 02-Java-Syntax-Homework/src/P01RectangleArea.java
+++ 02-Java-Syntax-Homework/src/P01RectangleArea.java
@@ -4,16 +4,15 @@ public class P01RectangleArea {
 	public static void main(String[] args) {
 		
 		int a = 0, b = 0, area = 0;
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		try {
-			
+			a = scan.nextInt();
+			b = scan.nextInt();
+			area = a * b;
+			System.out.println(area);
 		} catch (NumberFormatException nfex) {
 			System.err.println("Fail to scan number " + nfex);
-		}
-		a = scan.nextInt();
-		b = scan.nextInt();	
-		scan.close();
-		area = a * b;
-		System.out.println(area);
+		}							
 	}
 }
