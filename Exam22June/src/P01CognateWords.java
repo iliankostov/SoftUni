e50db83696f2ad6f52dc805900b4e71cbaddf264
ilian <ilian.kostov@gmail.com>
2014-09-20 20:02:04 +0300

    Java Practice

---
+++ Exam22June/src/P01CognateWords.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class P01CognateWords {
+
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		String[] words = input.split("\\W+");
+		boolean check = false;
+		HashSet<String> cognateWords = new HashSet<>();
+		for (int a = 0; a < words.length; a++) {
+			for (int b = 0; b < words.length; b++) {
+				for (int c = 0; c < words.length; c++) {
+					if (a!=b) {
+						check = (words[a] + words[b]).equals(words[c]);
+						if (check) {
+							cognateWords.add(words[a] + "|" + words[b] + "=" + words[c]);
+						}
+					}
+				}
+				
+			}
+			
+		}
+		if (!cognateWords.isEmpty()) {
+			for (String cognate : cognateWords) {
+				System.out.println(cognate);
+			}
+		}
+		else {
+			System.out.println("No");
+		}
+		
+	}
+
+}
