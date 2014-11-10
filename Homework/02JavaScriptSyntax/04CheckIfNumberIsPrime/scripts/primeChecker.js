5d3f12df83fdffcc8daaaafd3d3a734a89cbebf3
Ilian Kostov <ilian.kostov@gmail.com>
2014-11-10 22:11:45 +0200

    JavaScript Homework

---
+++ Homework/02JavaScriptSyntax/04CheckIfNumberIsPrime/scripts/primeChecker.js
@@ -0,0 +1,18 @@
+﻿function isPrimeNumber(number) {
+    if (number < 2) {
+        return false;
+    } else if (number == 2) {
+        return true;
+    } else {
+        for (var i = 2; i <= Math.sqrt(number) ; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+console.log(isPrimeNumber(7));
+console.log(isPrimeNumber(254));
+console.log(isPrimeNumber(587));
