9132e74b59cdbe17fedcaf1e99a51e8830fd81d6
Ilian Kostov <ilian.kostov@gmail.com>
2014-11-15 11:10:31 +0200

    JavaScript Homework

---
+++ Homework/03JavaScriptLoopsArraysStrings/01NumberChecker/scripts/numberChecker.js
@@ -0,0 +1,18 @@
+function printNumbers(number) {
+    if (number < 1) {
+        return console.log('no');
+    }
+    var digits = [];
+    for (var i = 1; i <= number; i++) {
+        if ((i % 4 == 0) || (i % 5 == 0))  {
+            continue;
+        } else {
+            digits.push(i);
+        }
+    }
+    return console.log(digits.join(', '));
+}
+
+printNumbers(20);
+printNumbers(-5);
+printNumbers(13);
