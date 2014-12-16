78317c31a942cb9499fd3e95e4e225975a2a193f
Ilian Kostov <ilian.kostov@gmail.com>
2014-12-16 18:16:04 +0200

    PHP Basics Homework

---
+++ Homework/05PHPArraysStringAndObjects/04TextFilter/TextFilter.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Text Filter</title>
+    <link rel="stylesheet" href="styles/styles.css"/>
+</head>
+<body>
+<main>
+    <section>
+        <form method="post">
+            <textarea name="text" title="Enter text"></textarea>
+            <input type="text" name="banlist" title="Enter banlist"/>
+            <input type="submit" name="submit" value="Filter text"/>
+        </form>
+    </section>
+    <section>
+        <article>
+            <?php
+            if (isset($_POST['submit'])):
+                $text = $_POST['text'];
+                $banList = preg_split('/, /', $_POST['banlist']);
+                foreach ($banList as $word) {
+                    $asterisk = str_repeat('*', strlen($word));
+                    $text = str_ireplace($word,$asterisk,$text);
+                }
+                ?>
+                <p><?php echo $text ?></p>
+            <?php endif; ?>
+        </article>
+    </section>
+</main>
+</body>
+</html>
