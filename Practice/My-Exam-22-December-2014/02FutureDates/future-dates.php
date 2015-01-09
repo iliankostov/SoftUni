ba2104686fc848ce38cf6d28f82af789e4dc983d
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-10 01:56:24 +0200

    PHP Basics Practice

--- Practice/My-Exam-22-December-2014/02FutureDates/future-dates.php
+++ Practice/My-Exam-22-December-2014/02FutureDates/future-dates.php
@@ -1,10 +1,8 @@
 <?php
-date_default_timezone_set('Europe/Sofia');
-
 $numbersString = htmlentities($_GET['numbersString']);
 $dateString = htmlentities($_GET['dateString']);
 
-$patternNum = '/[^A-Za-z0-9]?(\d*)[^A-Za-z0-9]?/';
+$patternNum = '/[^A-Za-z0-9]+?(\d+)[^A-Za-z0-9]+?/';
 preg_match_all($patternNum, $numbersString, $numbers, PREG_SPLIT_NO_EMPTY);
 
 $sum = 0;
@@ -13,16 +11,23 @@ foreach ($numbers[1] as $number) {
     $sum += $number;
 }
 
-$backwards_sum = strrev($sum);
+$backward_sum = strrev($sum);
 
 $patternDate = '/(\d{4})-(\d{2})-(\d{2})/';
 preg_match_all($patternDate, $dateString, $dates);
 
-if ($dates[0]) {
+if (!empty($dates[0])) {
+    $newDates = [];
+
+    foreach ($dates[0] as $date) {
+        $tempDate = date_create($date, timezone_open("Europe/Sofia"));
+        date_add($tempDate, date_interval_create_from_date_string("$backward_sum days"));
+        array_push($newDates, $tempDate);
+    }
 
     echo "<ul>";
-    foreach ($dates[0] as $date) {
-        echo "<li>" . date('Y-m-d', strtotime("+$backwards_sum day", strtotime($date))) . "</li>";
+    foreach ($newDates as $newDate) {
+        echo "<li>" . date_format($newDate, "Y-m-d") . "</li>";
     }
     echo "</ul>";
 
