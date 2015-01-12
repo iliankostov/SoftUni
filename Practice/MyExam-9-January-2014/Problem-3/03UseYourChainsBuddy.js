ab284eb6f730bfe5978b1ed27103b18515b950e3
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-12 21:25:21 +0200

    JavaScript Practice

--- Practice/MyExam-9-January-2014/Problem-3/03UseYourChainsBuddy.js
+++ Practice/MyExam-9-January-2014/Problem-3/03UseYourChainsBuddy.js
@@ -1,59 +1,63 @@
-function solve(arr) {
+function useYourChains(array) {
+    var input = array[0];
+    var pattern = /<p>(.*?)<\/p>/g;
+    var matches = pattern.exec(input);
+    var matches_array = [];
 
-    var str = arr[0];
-    var paragraphs = [];
-    var m, rex = /<p>(.*?)<\/p>/g;
-
-    while ( ( m = rex.exec( str ) ) != null ) {
-        paragraphs.push( m[1] );
+    while (matches != null) {
+        var match = matches[1];
+        matches_array.push(match);
+        matches = pattern.exec(input);
     }
 
-    var output = [];
+    var output_string = '';
 
-    for (var a in paragraphs) {
-        if (paragraphs.hasOwnProperty(a)) {
-            var tempParagraph = paragraphs[a];
-            var paragraph = "";
-            for (var c in tempParagraph) {
-                if (tempParagraph.hasOwnProperty(c)) {
-                    var ch = tempParagraph[c];
-                    var charPatern = /[a-z0-9]/g;
-                    if (ch.match(charPatern)) {
-                        paragraph += ch;
-                    } else{
-                        paragraph += " ";
-                    }
+    for (var a in matches_array) {
+        if (matches_array.hasOwnProperty(a)) {
+            var characters = '';
+
+            for (var b = 0; b < matches_array[a].length; b++) {
+
+                var char = matches_array[a][b];
+                var char_pattern = /[a-z0-9]/g;
+                var char_match = char.match(char_pattern);
+
+                if (char_match) {
+                    characters += char;
                 }
-            }
-            paragraph = paragraph.replace(/\s+/g, " ").trim();
-
-            var convertedParagraph = "";
-
-            for (var e in paragraph) {
-                if (paragraph.hasOwnProperty(e)) {
-                    var charToConvert = paragraph[e];
-                    if (!isNaN(charToConvert)) {
-                        convertedParagraph += charToConvert;
-                    } else if (charToConvert.charCodeAt(0) >= 97 && charToConvert.charCodeAt(0) <= 109) {
-                        convertedParagraph += String.fromCharCode(charToConvert.charCodeAt(0) + 13);
-                    } else if (charToConvert.charCodeAt(0) >= 110 && charToConvert.charCodeAt(0) <= 122) {
-                        convertedParagraph += String.fromCharCode(charToConvert.charCodeAt(0) - 13);
-                    } else if (charToConvert === "\n") {
-                        console.log('error');
-                    }
+                else {
+                    characters += ' ';
                 }
-            }
 
-            output.push(convertedParagraph);
+            }
+            output_string += characters;
         }
     }
-    console.log(output.join(" "));
+    output_string = output_string.replace(/\s+/g, ' ');
+    var final_output = '';
+
+    for (var c = 0; c < output_string.length; c++) {
+
+        var current_char_code = output_string.charCodeAt(c);
+
+        if (current_char_code >= 97 && current_char_code <= 109) {
+            final_output += String.fromCharCode(current_char_code + 13);
+        }
+        else if (current_char_code >= 110 && current_char_code <= 122) {
+            final_output += String.fromCharCode(current_char_code - 13);
+        }
+        else {
+            final_output += String.fromCharCode(current_char_code);
+        }
+    }
+
+    console.log(final_output);
 }
 
-solve([
+useYourChains([
 '\<html><head><title></title></head><body><h1>hello</h1><p>znahny!@#%&&&&****</p><div><button>dsad</button></div><p>grkg^^^^%%%)))([]12</p></body></html>'
 ]);
 
-solve([
+useYourChains([
 '\<html><head><title></title></head><body><h1>Intro</h1><ul><li>Item01</li><li>Item02</li><li>Item03</li></ul><p>jura qevivat va jrg fyvccrel fabjl</p><div><button>Click me, baby!</button></div><p> pbaqvgvbaf fabj  qpunvaf ner nofbyhgryl rffragvny sbe fnsr unaqyvat nygubhtu fabj punvaf znl ybbx </p><span>This manual is false, do not trust it! The illuminati wrote it down to trick you!</span><p>vagvzvqngvat gur onfvp vqrn vf ernyyl fvzcyr svg gurz bire lbhe gverf qevir sbejneq fybjyl naq gvtugra gurz hc va pbyq jrg</p><p> pbaqvgvbaf guvf vf rnfvre fnvq guna qbar ohg vs lbh chg ba lbhe gverf</p></body>'
 ]);
