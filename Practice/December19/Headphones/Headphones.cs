6b4b73767756d00f1ff760e639f9bca63be92f13
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-13 23:05:17 +0200

    C# Practice

---
+++ Practice/December19/Headphones/Headphones.cs
@@ -0,0 +1,62 @@
+﻿using System;
+class Headphones
+{
+    static void Main()
+    {
+        int diameter = int.Parse(Console.ReadLine());
+        char asterisk = '*';
+        char dash = '-';
+        
+
+        
+        int outsideDash = ((diameter * 2 + 1) - (diameter + 2)) / 2;
+        int insideDash = diameter;
+        int increaseHeadphones = 1;
+        int decreaseHeadphones = diameter;
+
+        for (int i = 0; i < diameter*2; i++)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine(new String(dash, outsideDash) + 
+                    new String(asterisk, diameter + 2) + new String(dash, outsideDash));
+            }
+            if (i < diameter - 1)
+            {
+                Console.WriteLine(new String(dash, outsideDash) + asterisk +
+                    new String(dash, insideDash) + asterisk + new String(dash, outsideDash));
+            }
+            if (i == diameter)
+            {
+                Console.WriteLine(new String(dash, outsideDash) + asterisk +
+                    new String(dash, insideDash) + asterisk + new String(dash, outsideDash));
+                outsideDash -= 1;
+                insideDash -= 2;
+                increaseHeadphones += 2;
+            }
+            if (i > diameter && i < (diameter + diameter / 2))
+            {
+                Console.WriteLine(new String(dash, outsideDash) + new String(asterisk, increaseHeadphones) +
+                    new String(dash, insideDash) + new String(asterisk, increaseHeadphones) + new String(dash, outsideDash));
+                outsideDash -= 1;
+                insideDash -= 2;
+                increaseHeadphones += 2;
+            }
+            if (i == (diameter + diameter / 2) + 1)
+            {
+                Console.WriteLine(new String(asterisk, diameter) + dash + new String(asterisk, diameter));
+                decreaseHeadphones -= 2;
+                insideDash += 2;
+                outsideDash += 1;
+            }
+            if (i > (diameter + diameter / 2) && i < diameter * 2)
+            {
+                Console.WriteLine(new String(dash, outsideDash) + new String(asterisk, decreaseHeadphones) +
+                    new String(dash, insideDash) + new String(asterisk, decreaseHeadphones) + new String(dash, outsideDash));
+                decreaseHeadphones -= 2;
+                insideDash += 2;
+                outsideDash += 1;
+            }
+        }
+    }
+}
---
+++ Practice/December19/SpyHard/SpyHard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class SpyHard
+{
+    static void Main()
+    {
+        int key = int.Parse(Console.ReadLine());
+        string message = Console.ReadLine();
+
+        string output = key + "" + message.Length;
+        int sum = 0;
+
+        char[] chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        string convertedNumber = "";
+
+        for (int a = 0; a < message.Length; a++)
+        {
+            if (((int)message[a] >= 97 && (int)message[a] <= 122) || ((int)message[a] >= 65 && (int)message[a] <= 90))
+            {
+                char ch = Char.ToLower(message[a]);
+
+                int chNum = (int)ch - 96;
+                sum += chNum;
+            }
+            else
+            {
+                int chNum = (int)message[a];
+                sum += chNum;
+            }
+        }
+
+        while (sum > 0)
+        {
+            convertedNumber = chars[sum % key] + convertedNumber;
+            sum = sum / key;
+        }
+
+        Console.WriteLine(output + convertedNumber);
+    }
+
+}
