cd7598b29aa2df6a5069f3e30c79d891b5e96970
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-13 21:50:01 +0200

    C# Practice

---
+++ Practice/December19/TravellerBob/TravellerBob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class TravellerBob
+{
+    static void Main()
+    {
+        string leapYear = Console.ReadLine();
+        int contractMonth = int.Parse(Console.ReadLine());
+        int familyMonth = int.Parse(Console.ReadLine());
+
+        int contractMonthsTravels = contractMonth * 12;
+        int familyMonthsTravels = familyMonth * 4;
+        double normalMonthsTravels = ((12 - contractMonth - familyMonth) * 12 * 1.0) * 3 / 5;
+
+        double totalTravels = (contractMonthsTravels) + (familyMonthsTravels) + (normalMonthsTravels);
+
+        if (leapYear == "leap")
+        {
+            totalTravels += totalTravels * 0.05;
+            Console.WriteLine((int)totalTravels);
+        }
+        else
+        {
+            Console.WriteLine((int)totalTravels);
+
+        }
+    }
+}
