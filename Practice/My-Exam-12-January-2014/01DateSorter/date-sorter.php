62ba9645c83ecac6503cdf9e75640a7ce32fc32e
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-14 19:49:36 +0200

    PHP Basics Practice

--- Practice/My-Exam-12-January-2014/01DateSorter/date-sorter.php
+++ Practice/My-Exam-12-January-2014/01DateSorter/date-sorter.php
@@ -2,35 +2,31 @@
 date_default_timezone_set("Europe/Sofia");
 
 $list = $_GET['list'];
-$currDate = $_GET['currDate'];
+$currDate = date_create($_GET['currDate']);
 
-$dates = preg_split('/\r\n/', $list, -1, PREG_SPLIT_NO_EMPTY);
-
-$currDate = strtotime($currDate);
-
+$dates = preg_split('/\n/', $list);
 $int_dates = [];
 
 foreach ($dates as $date) {
-    $date = trim($date);
-    $date = strtotime($date);
+    $date = date_create($date);
 
-    if ($date) {
+    if ($date != "" && $date) {
         $int_dates[] = $date;
     }
 }
 
-asort($int_dates);
+sort($int_dates);
 
 $output_str = "<ul>";
 
 foreach ($int_dates as $int_date) {
 
-    $post_date = htmlspecialchars(date("d/m/Y", $int_date));
+    $post_date = date_format($int_date, "d/m/Y");
 
     if ($int_date < $currDate) {
         $output_str .= "<li><em>$post_date</em></li>";
 
-    } elseif ($int_dates > $currDate) {
+    } else {
         $output_str .= "<li>$post_date</li>";
     }
 
