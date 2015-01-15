73ee8ad0390adda5d10c53ea0c852fd8de0cb5ff
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-15 17:27:41 +0200

    C# Practice

---
+++ Practice/December19/SpiralMatrix/SpiralMatrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class SpiralMatrix
+{
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        char[] chars = new char[size*size];
+
+        for (int a = 0, b = 0; a < size*size; a++, b++)
+        {
+            if (b > input.Length - 1)
+            {
+                b = 0;
+            }
+            chars[a] = input[b];
+
+        }
+        Console.WriteLine(string.Join(", ", chars.Select(v => v.ToString())));
+    }
+}
