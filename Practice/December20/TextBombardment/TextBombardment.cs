1e64a0c8d05f20ff177ed2ea472f2ad12414224f
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-17 19:19:42 +0200

    C# Practice

--- Practice/December20/TextBombardment/TextBombardment.cs
+++ Practice/December20/TextBombardment/TextBombardment.cs
@@ -3,6 +3,20 @@ class TextBombardment
 {
     static void Main()
     {
+        int tankPrice = int.Parse(Console.ReadLine());
+        int numberParties = int.Parse(Console.ReadLine());
 
+        int monthBalance = ((30 - numberParties) * 2) - (numberParties * 5);
+        if (monthBalance > 0)
+        {
+            decimal monthsNeeded = Math.Ceiling((decimal)tankPrice / (decimal)monthBalance);
+            int years = (int)(monthsNeeded / 12);
+            int months = (int)(monthsNeeded % 12);
+            Console.WriteLine("{0} years, {1} months", years, months);
+        }
+        else
+        {
+            Console.WriteLine("never");
+        }
     }
 }
