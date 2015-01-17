c3dbb44959ed2f58e25ed4374b72609e326b4dfb
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-17 19:50:47 +0200

    C# Practice

--- Practice/December20/CheatSheet/CheatSheet.cs
+++ Practice/December20/CheatSheet/CheatSheet.cs
@@ -3,6 +3,22 @@ class CheatSheet
 {
     static void Main()
     {
+        int rows = int.Parse(Console.ReadLine());
+        int cols = int.Parse(Console.ReadLine());
+        long vStart = int.Parse(Console.ReadLine());
+        long hStart = int.Parse(Console.ReadLine());
 
+        for (long i = vStart; i < vStart + rows; i++)
+        {
+            for (long j = hStart; j < hStart + cols; j++)
+            {
+                Console.Write(i * j);
+                if (j < hStart + cols - 1)
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
     }
 }
