5f06093abb9c9be5258dbf51f460ce49d964ab8f
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-18 15:55:22 +0200

    C# Practice

--- Practice/Understanding-OOP/LetTheCatsSayMiau.cs
+++ Practice/Understanding-OOP/LetTheCatsSayMiau.cs
@@ -5,27 +5,47 @@ public class Cat    // Beginning of class definition
     private string name;    // Field 1
     private string owner;   // Field 2
 
-    public Cat(string name, string owner)   // Beginning of constructor definition
-    {
+    public Cat(string name = null, string owner = null)   // Beginning of constructor definition.
+    {                                                     // If we equal property to null it won't be required.
         this.name = name;       // Constructor's property 1
         this.owner = owner;     // Constructor's property 2
     }
 
-    public string Name      // Create template for property "Name"
+    public string Name      // Create public property "Name"
     {
-        get { return this.name; }       // get the name of the cat template
-        set { this.name = value; }      // set it a string value
+        get { return this.name; }       // get the name of the cat
+        set
+        {
+            if (true)                   // Here we can validate the input.
+            {
+                this.name = value;      // set a string value to name property.
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("The name is out of range");
+            }
+        }
     }
 
-    public string Owner     // Create template for property "Owner"
+    public string Owner     // Create public property "Owner"
     {
-        get { return this.owner; }      // get the owner of the cat template
-        set { this.owner = value; }     // set it a string value
+        get { return this.owner; }      // get the owner of the cat
+        set
+        {
+            if (true)                   // Here we can validate the input.
+            {
+                this.owner = value;     // set a string value to owner property. 
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("The owner is out of range");
+            }
+        }
     }
 
-    public void SayMiau()       // Create method (function) for dinamic of the cat
+    public void SayMiau()       // Create public method (function) for dinamic of the cat
     {
-        Console.Write("Miauuuuuuu ");       // This is the dinamic result
+        Console.Write("Miauuuuuuu ");       // This is dinamic result
     }
 
 }
@@ -34,7 +54,7 @@ class LetTheCatsSayMiau
 {
     static void Main()
     {
-        Cat catOne = new Cat("", "");    // Create a new cat
+        Cat catOne = new Cat();    // Create a new cat
         Cat catTwo = new Cat("Pursy", "Andrew");    // Create another cat
 
         catOne.Name = "Kitty";     // Change the name of the cat
