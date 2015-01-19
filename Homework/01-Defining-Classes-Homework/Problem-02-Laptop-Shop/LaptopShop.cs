c1585a19cb292c156757fb3bf06c5bd2fd1052ad
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-19 23:15:53 +0200

    C# OOP Homework

--- Homework/01-Defining-Classes-Homework/Problem-02-Laptop-Shop/LaptopShop.cs
+++ Homework/01-Defining-Classes-Homework/Problem-02-Laptop-Shop/LaptopShop.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 class Laptop
 {
@@ -11,19 +12,29 @@ class Laptop
     private ushort hdd;
     private string screen;
     private decimal price;
+    private Battery battery;
 
     // Create first constructor
-    public Laptop(string model, string manufacturer, string processor, byte ram, string graphicsCard,
-        ushort hdd, string screen, string battery, float batteryLife, decimal price)
+    public Laptop(string model, decimal price)
     {
         this.Model = model;
+        this.Price = price;
+    }
+
+    // Create second constructor
+    public Laptop(string model, decimal price, string manufacturer = null, string processor = null, 
+        byte ram = 0, string graphicsCard = null, ushort hdd = 0, string screen = null, 
+        string batteryType = null, float batteryLife = 0F) 
+        : this (model, price)
+    {
+        
         this.Manufacturer = manufacturer;
         this.Processor = processor;
         this.Ram = ram;
         this.GraphicsCard = graphicsCard;
         this.Hdd = hdd;
         this.Screen = screen;
-        this.Price = price;
+        this.Battery = new Battery(batteryType, batteryLife);
     }
 
     // Create and validate property Model
@@ -138,25 +149,82 @@ class Laptop
         }
     }
 
-}
-
-class Battery : Laptop
-{
-    // Create fields
-    private string battery;
-    private float batteryLife;
-
     // Create and validate property Battery
-    public string BatteryProp
+    public Battery Battery
     {
         get { return this.battery; }
+        set { this.battery = value; }
+    }
+
+    // Create override the ToString() method
+    public override string ToString()
+    {
+        StringBuilder laptopStringBuild = new StringBuilder();
+        laptopStringBuild.AppendLine("model: " + this.Model);
+        if (this.Manufacturer != null)
+        {
+            laptopStringBuild.AppendLine("manufacturer: " + this.Manufacturer);
+        }
+        if (this.Processor != null)
+        {
+            laptopStringBuild.AppendLine("processor: " + this.Processor);
+        }
+        if (this.Ram != 0)
+        {
+            laptopStringBuild.AppendLine("RAM: " + this.Ram + " GB");
+        }
+        if (this.GraphicsCard != null)
+        {
+            laptopStringBuild.AppendLine("graphics card: " + this.GraphicsCard);
+        }
+        if (this.Hdd != 0)
+        {
+            laptopStringBuild.AppendLine("HDD: " + this.Hdd + "GB SSD");
+        }
+        if (this.Screen != null)
+        {
+            laptopStringBuild.AppendLine("screen: " + this.Screen);
+        }
+        if (this.Battery != null)
+        {
+            laptopStringBuild.AppendLine(this.Battery.ToString());
+        }
+        laptopStringBuild.AppendLine("price: " + this.Price + " lv");
+        return laptopStringBuild.ToString();
+    }
+
+}
+
+class Battery
+{
+    // Create fields
+    private string batteryType;
+    private float batteryLife;
+
+    // Create first constructor
+    public Battery(string batteryType = null)
+    {
+        this.BatteryType = batteryType;
+    }
+
+    // Create second constructor
+    public Battery(string batteryType = null, float batteryLife = 0F) 
+        : this(batteryType)
+    {
+        this.BatteryLife = batteryLife;
+    }
+
+    // Create and validate property BatteryType
+    public string BatteryType
+    {
+        get { return this.batteryType; }
         set
         {
             if (string.IsNullOrEmpty(value))
             {
                 throw new ArgumentNullException("The battery's field can't be empty");
             }
-            this.battery = value;
+            this.batteryType = value;
         }
     }
 
@@ -173,12 +241,35 @@ class Battery : Laptop
             this.batteryLife = value;
         }      
     }
+
+    // Create override the ToString() method
+    public override string ToString()
+    {
+        StringBuilder batteryStringBuild = new StringBuilder();
+        if (this.BatteryType != null)
+        {
+            batteryStringBuild.AppendLine("battery: " + this.BatteryType);
+        }
+        if (this.BatteryLife != 0F)
+        {
+            batteryStringBuild.Append("battery life: " + this.BatteryLife + " hours");
+        }
+        return batteryStringBuild.ToString();
+    }
 }
 
-class MainProgram
+class LaptopShop
 {
     static void Main()
     {
+        Laptop lenovo = new Laptop("Lenovo Yoga 2 Pro", 2259.00M, "Lenovo", 
+            "Intel Core i5-4210U (2-core, 1.70 - 2.70 GHz, 3MB cache)", 
+            8, "Intel HD Graphics 4400", 128, "13.3\"(33.78 cm) – 3200 x 1800(QHD +), IPS sensor display",
+            "Li-Ion, 4-cells, 2550 mAh", 4.5F);
 
+        Laptop hp = new Laptop("HP 250 G2", 699.00M);
+
+        Console.WriteLine(lenovo);
+        Console.WriteLine(hp);
     }
 }
