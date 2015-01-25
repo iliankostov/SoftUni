502f38b03185275e133cac3107998ac13f5f9ee1
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-25 22:24:03 +0200

    Solve problem 01

--- Homework/03-Exception-Handling-Homework/01SquareRoot/SquareRootMain.cs
+++ Homework/03-Exception-Handling-Homework/01SquareRoot/SquareRootMain.cs
@@ -1,10 +1,36 @@
 ﻿namespace SquareRoot
 {
+    using System;
     class SquareRootMain
     {
         static void Main()
         {
-
+            Console.Write("Please enter positive integer number: ");
+            string input = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(input);
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                double sqrtNum = Math.Sqrt(number);
+                Console.WriteLine("The square root of {0} is {1:0.00}",number, sqrtNum);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number.");
+                Console.WriteLine("The problem is that you enter non-integer number.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number.");
+                Console.WriteLine("The problem is that you enter negative number.");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
         }
     }
 }
