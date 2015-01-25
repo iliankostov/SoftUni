6a2da2547ef47c9515f9ed1b2bc4e643a2066214
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-25 20:30:16 +0200

    Solve problem 01

---
+++ Homework/02-Static-Members-and-Namespaces-Homework/01Point3D/Point3D.cs
@@ -0,0 +1,74 @@
+﻿namespace Point3D
+{
+    using System;
+    class Point3D
+    {
+        // Create fields
+        private double x;
+        private double y;
+        private double z;
+        private static readonly Point3D startingPoint3D = new Point3D(0, 0, 0);
+
+        // Create constructor
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        // Create property X
+        public double X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        // Create property Y
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+
+        // Create property Z
+        public double Z
+        {
+            get
+            {
+                return this.z;
+            }
+            set
+            {
+                this.z = value;
+            }
+        }
+
+        // Create static property StartingPoint3D
+        public static Point3D StartingPoint3D
+        {
+            get
+            {
+                return startingPoint3D;
+            }
+        }
+
+        // Override toString() method
+        public override string ToString()
+        {
+            return string.Format("x = {0:0.00}; y = {1:0.00}; z = {2:0.00}", this.X, this.Y, this.Z);
+        }
+    }
+}
--- Homework/02-Static-Members-and-Namespaces-Homework/01Point3D/Point3DMain.cs
+++ Homework/02-Static-Members-and-Namespaces-Homework/01Point3D/Point3DMain.cs
@@ -5,7 +5,21 @@
     {
         static void Main()
         {
+            Point3D pointZero = Point3D.StartingPoint3D;
 
+            Point3D pointOne = new Point3D(1.0, 1.0, 1.0);
+            Point3D pointTwo = new Point3D(2.0, 2.0, 2.0);
+
+            Random r = new Random();
+            double rand = r.NextDouble() * (100 - 0) + 0;
+            Point3D pointRandom = new Point3D(rand, rand, rand);
+
+            Point3D[] points = new Point3D[] { pointZero, pointOne, pointTwo, pointRandom };
+
+            foreach (Point3D point in points)
+            {
+                Console.WriteLine(point);
+            }
         }
     }
 }
