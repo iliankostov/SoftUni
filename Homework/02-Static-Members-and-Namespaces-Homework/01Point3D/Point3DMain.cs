cb3e909a2ba22801f7a2f809cc4bab47d92d72da
Ilian Kostov <ilian.kostov@gmail.com>
2015-01-28 22:54:59 +0200

    Solve the problem

--- Homework/02-Static-Members-and-Namespaces-Homework/01Point3D/Point3DMain.cs
+++ Homework/02-Static-Members-and-Namespaces-Homework/01Point3D/Point3DMain.cs
@@ -11,11 +11,7 @@
             Point3D pointOne = new Point3D(1.0, 1.0, 1.0);
             Point3D pointTwo = new Point3D(2.0, 2.0, 2.0);
 
-            Random r = new Random();
-            double rand = r.NextDouble() * (100 - 0) + 0;
-            Point3D pointRandom = new Point3D(rand, rand, rand);
-
-            Point3D[] points = new Point3D[] { pointZero, pointOne, pointTwo, pointRandom };
+            Point3D[] points = new Point3D[] { pointZero, pointOne, pointTwo };
 
             foreach (Point3D point in points)
             {
---
+++ Homework/02-Static-Members-and-Namespaces-Homework/03Paths/Path3D.cs
@@ -0,0 +1,18 @@
+﻿namespace Paths
+{
+    using System;    
+    using System.Collections.Generic;
+    using Points3D;
+
+    public class Path3D
+    {
+        // Create constructor
+        public Path3D(List<Point3D> paths)
+        {
+            this.Paths = paths;
+        }
+
+        // Create readonly property
+        public List<Point3D> Paths { get; private set; }
+    }
+}
