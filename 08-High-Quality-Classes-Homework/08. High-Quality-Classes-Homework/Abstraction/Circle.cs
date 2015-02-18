367f55e40eaf99c094cdbdca312969a85cbf22f1
Ilian Kostov <ilian.kostov@gmail.com>
2015-02-18 22:01:14 +0200

    Solve homework 08 problem 01

---
+++ 08-High-Quality-Classes-Homework/08. High-Quality-Classes-Homework/Abstraction/Circle.cs
@@ -0,0 +1,26 @@
+﻿namespace Abstraction
+{
+    using System;
+
+    internal class Circle : Figure
+    {      
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public virtual double Radius { get; set; }
+
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * Math.PI * this.Radius;
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = Math.PI * this.Radius * this.Radius;
+            return surface;
+        }
+    }
+}
---
+++ 08-High-Quality-Classes-Homework/08. High-Quality-Classes-Homework/Abstraction/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace Abstraction
+{
+    internal class Rectangle : Figure
+    {
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public virtual double Width { get; set; }
+
+        public virtual double Height { get; set; }
+
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * (this.Width + this.Height);
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = this.Width * this.Height;
+            return surface;
+        }
+    }
+}
