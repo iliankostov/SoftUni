cf7f0d9a9d9e6a7cd15b2c8eb666da5506ab1eb4
Ilian Kostov <ilian.kostov@gmail.com>
2015-02-18 23:00:07 +0200

    Solve problem 04 Inheritance and Polymorphism

---
+++ 08-High-Quality-Classes-Homework/08. High-Quality-Classes-Homework/Inheritance-and-Polymorphism/Models/LocalCourse.cs
@@ -0,0 +1,68 @@
+﻿namespace InheritanceAndPolymorphism.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class LocalCourse : Course
+    {
+        private string lab;
+
+        public LocalCourse(string courseName)
+            : base(courseName)
+        {
+        }
+
+        public LocalCourse(string courseName, string teacherName)
+            : base(courseName, teacherName)
+        {
+        }
+
+        public LocalCourse(string courseName, string teacherName, IList<string> students)
+            : base(courseName, teacherName, students)
+        {
+        }
+
+        public LocalCourse(string courseName, string teacherName, IList<string> students, string lab)
+            : base(courseName, teacherName, students)
+        {
+            this.Lab = lab;
+        }
+
+        public string Lab
+        {
+            get
+            {
+                return this.lab;
+            }
+
+            protected internal set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(
+                        "Invalid local course lab name caught at InheritanceAndPolymorphism.LocalCourse.Lab!" +
+                        "Local course lab name cannot be null, empty, or consist only of white-space characters.");
+                }
+
+                this.lab = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder localCourseInfo = new StringBuilder();
+            localCourseInfo.Append("LocalCourse { Name = ");
+            localCourseInfo.Append(base.ToString());
+            if (this.Lab != null)
+            {
+                localCourseInfo.Append("; Lab = ");
+                localCourseInfo.AppendFormat("{0};", this.Lab);
+            }
+
+            localCourseInfo.Append(" }");
+
+            return localCourseInfo.ToString();
+        }
+    }
+}
---
+++ 08-High-Quality-Classes-Homework/08. High-Quality-Classes-Homework/Inheritance-and-Polymorphism/Models/OffsiteCourse.cs
@@ -0,0 +1,68 @@
+﻿namespace InheritanceAndPolymorphism.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class OffsiteCourse : Course
+    {
+        private string town;
+
+        public OffsiteCourse(string courseName)
+            : base(courseName)
+        {
+        }
+
+        public OffsiteCourse(string courseName, string teacherName)
+            : base(courseName, teacherName)
+        {
+        }
+
+        public OffsiteCourse(string courseName, string teacherName, IList<string> students)
+            : base(courseName, teacherName, students)
+        {
+        }
+
+        public OffsiteCourse(string courseName, string teacherName, IList<string> students, string town)
+            : base(courseName, teacherName, students)
+        {
+            this.Town = town;
+        }
+
+        public string Town
+        {
+            get
+            {
+                return this.town;
+            }
+
+            protected internal set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(
+                        "Invalid offsite course town name caught at InheritanceAndPolymorphism.OffsiteCourse.Town!" +
+                        "Offsite course town name cannot be null, empty, or consist only of white-space characters.");
+                }
+
+                this.town = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder offsiteCourseInfo = new StringBuilder();
+            offsiteCourseInfo.Append("OffsiteCourse { Name = ");
+            offsiteCourseInfo.Append(base.ToString());
+            if (this.Town != null)
+            {
+                offsiteCourseInfo.Append("; Town = ");
+                offsiteCourseInfo.AppendFormat("{0};", this.Town);
+            }
+
+            offsiteCourseInfo.Append(" }");
+
+            return offsiteCourseInfo.ToString();
+        }
+    }
+}
