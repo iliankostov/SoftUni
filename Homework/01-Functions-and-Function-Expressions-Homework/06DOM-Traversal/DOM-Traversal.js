c52576f84350b8869e09f98c755a0d51d2cee71d
Ilian Kostov <ilian.kostov@gmail.com>
2015-03-09 20:07:23 +0200

    Solve problem 06 DOM Traversal

--- Homework/01-Functions-and-Function-Expressions-Homework/06DOM-Traversal/DOM-Traversal.js
+++ Homework/01-Functions-and-Function-Expressions-Homework/06DOM-Traversal/DOM-Traversal.js
@@ -2,17 +2,25 @@ function traverse(node) {
     traverseNode(node, '');
 
     function traverseNode(node, spacing) {
+        this.htmlElement = node;
         spacing = spacing || '  ';
-        console.log(spacing + node.nodeName);
+        var tagName = this.htmlElement.tagName.toLowerCase();
+        var elementId = this.htmlElement.id;
+        var elementClass = this.htmlElement.className;
+        var output = spacing + tagName + ": ";
+        output += elementId != '' ? 'id="' + elementId + '"' : '';
+        output += elementClass != '' ? 'class="' + elementClass +'"' : '';
+        console.log(output);
+
         for (var i = 0, len = node.childNodes.length; i < len; i += 1) {
             var child = node.childNodes[i];
             if (child.nodeType === document.ELEMENT_NODE) {
                 traverseNode(child, spacing + '  ');
             }
         }
-        console.log(spacing + '/' + node.nodeName);
     }
 }
 
+console.log("The correct answer is in the console of the browser!");
 var selector = ".birds";
-traverse(document.getElementsByName(selector));
+traverse(document.querySelector(selector));
