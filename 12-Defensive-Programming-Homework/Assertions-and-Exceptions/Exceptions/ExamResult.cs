bb00ebbb4d57a2d7e69cc082128834a12030ab89
Ilian Kostov <ilian.kostov@gmail.com>
2015-03-11 20:25:08 +0200

    Solve problem 02 Exceptions

---
+++ 12-Defensive-Programming-Homework/Assertions-and-Exceptions/Exceptions/ExamResult.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+public class ExamResult
+{
+    private int grade;
+    private int minGrade;
+    private int maxGrade;
+    private string comments;
+
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+
+    public int Grade
+    {
+        get
+        {
+            return this.grade;
+        }
+
+        private set
+        {
+            if (value < 0)
+            {
+                throw new Exception();
+            }
+
+            this.grade = value;
+        }
+    }
+
+    public int MinGrade
+    {
+        get
+        {
+            return this.minGrade;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new Exception();
+            }
+
+            this.minGrade = value;
+        }
+    }
+
+    public int MaxGrade
+    {
+        get
+        {
+            return this.maxGrade;
+        }
+
+        set
+        {
+            if (value <= this.minGrade)
+            {
+                throw new Exception();
+            }
+
+            this.maxGrade = value;
+        }
+    }
+
+    public string Comments
+    {
+        get
+        {
+            return this.comments;
+        }
+
+        private set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception();
+            }
+
+            this.comments = value;
+        }
+    }
+}
