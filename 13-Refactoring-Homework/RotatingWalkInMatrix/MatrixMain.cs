ac88408053c815cd33e735a494373f713bc05a14
Ilian Kostov <ilian.kostov@gmail.com>
2015-03-11 22:03:13 +0200

    Solve homework 13 Refactoring

---
+++ 13-Refactoring-Homework/RotatingWalkInMatrix/MatrixMain.cs
@@ -0,0 +1,12 @@
+﻿namespace RotatingWalkInMatrix
+{
+    public static class MatrixMain
+    {
+        private static void Main()
+        {
+            var n = Matrix.ReadMatrixSize();
+            var matrix = Matrix.BuildMatrix(n);
+            Matrix.PrintMatrix(matrix);
+        }
+    }
+}
