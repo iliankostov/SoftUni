7fb4b4c33488453bd95f9945c4c97bcb99c7a2c3
Ilian Kostov <ilian.kostov@gmail.com>
2015-03-23 10:19:35 +0200

    Solve homework 04 problem 01 TODO List Module

--- Homework/04-JavaScript-Design-Patterns-Homework/01-TODO-List-Module/scripts/TODO-List-Module.js
+++ Homework/04-JavaScript-Design-Patterns-Homework/01-TODO-List-Module/scripts/TODO-List-Module.js
@@ -1,59 +1,6 @@
-var Todo = Todo || {};
-Todo.Container = (function () {
-    var Container = function(sections) {
-        this._sections = sections;
-    };
+var toDoList = toDoList || {};
 
-    Container.prototype.addToDOM = function(){
-        var container = document.createElement('div');
-        container.id = 'container';
-        document.getElementsByTagName('body')[0].appendChild(container);
-        var h1 = document.createElement('h1');
-        h1.innerHTML += 'Tuesday ';
-        var todo = document.createElement('em');
-        todo.innerHTML = 'TODO';
-        h1.appendChild(todo);
-        h1.innerHTML += ' list';
-        container.appendChild(h1);
-        var innerContainer = document.createElement('div');
-        innerContainer.id = 'inner-container';
+var list = toDoList._createToDoList('Tuesday TODO List'),
+    body = document.body;
 
-        // try to add sections into container
-        //innerContainer.appendChild(this._sections);
-
-        container.appendChild(innerContainer);
-        var form = document.createElement('form');
-        var input = document.createElement('input');
-        var button = document.createElement('button');
-        button.innerHTML = 'New Section';
-        container.appendChild(form);
-        form.appendChild(input);
-        form.appendChild(button);
-    };
-
-    return Container;
-})();
-
-Todo.Section = (function () {
-    var Section = function(title, items) {
-        this._title = title;
-        this._items = items;
-    };
-    
-    Section.prototype.addToDOM = function() {
-        var section = document.createElement('div');
-        document.getElementById('container').appendChild(section);
-        var sectionBox = document.createElement('div');
-        section.appendChild(sectionBox);
-        var h2 = document.createElement('h2');
-        h2.innerHTML = this._title;
-        sectionBox.appendChild(h2);
-
-    };
-
-    return Section;
-})();
-
-var section = new Todo.Section('Shopping List');
-var container = new Todo.Container(section);
-container.addToDOM();
+body.appendChild(list.getDomElement());
