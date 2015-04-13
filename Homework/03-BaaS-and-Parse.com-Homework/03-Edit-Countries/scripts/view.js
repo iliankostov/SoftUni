52117001b60bcce900afbe76b1cd1f1057c9448f
Ilian Kostov <ilian.kostov@gmail.com>
2015-04-13 23:54:56 +0300

    Try homework 03 problem 03 Edit Countries

---
+++ Homework/03-BaaS-and-Parse.com-Homework/03-Edit-Countries/scripts/view.js
@@ -0,0 +1,80 @@
+var world = world || {};
+
+world.View = (function() {
+    function View(model) {
+        this.model = model;
+        this.attachEventListeners(this);
+    }
+
+    View.prototype.showAllCountries = function () {
+        var _this = this;
+        this.model.countries.loadCountries(
+            function (countryData) {
+                countryData.results.forEach(function (country) {
+                    _this.addCountryToDom(country.name, country.objectId);
+                });
+            },
+            function (error) {
+                console.error(error.responseText);
+            }
+        )
+    };
+
+    View.prototype.addCountry = function () {
+        var _this = this;
+        var countryName = $('#country-name').val();
+        this.model.countries.addCountry({name: countryName},
+            function (data) {
+                _this.addCountryToDom(countryName, data.objectId);
+            },
+            function (error) {
+                console.log(error);
+            }
+        )
+    };
+    
+    View.prototype.deleteCountry = function (countryId) {
+        this.model.countries.deleteCountry(countryId,
+            function (data) {
+                $('#wrapper').find('[data-id=' + countryId + ']').remove();
+            },
+            function (error) {
+                console.log(error.responseText);
+            }
+        );
+    };
+
+    View.prototype.attachEventListeners = function (View) {
+        $('#add-country').click(function () {
+            View.addCountry(View);
+        });
+    };
+
+    View.prototype.addCountryToDom = function(country, countryId) {
+        var _this = this;
+        var countryWrapper = $('<div />').addClass('country-list');
+        countryWrapper.attr('data-id', countryId);
+        var countryName = $('<p/>').text(country);
+        var editInput = $('<input/>');
+        var editButton = $('<button class="edit-country">Edit</button>');
+        var deleteButton = $('<button class="delete-country">Delete</button>');
+
+        deleteButton.click(function () {
+            _this.deleteCountry(countryId);
+        });
+
+        countryWrapper
+            .append(countryName)
+            .append(editInput)
+            .append(editButton)
+            .append(deleteButton);
+
+        $("#wrapper").append(countryWrapper);
+    };
+
+    return {
+        loadView: function (model) {
+            return new View(model);
+        }
+    };
+})();
