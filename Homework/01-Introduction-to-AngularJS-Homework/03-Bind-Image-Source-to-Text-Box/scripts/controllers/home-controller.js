aeeaf67695cd40aa162c2e07bfc1a003b2353041
Ilian Kostov <ilian.kostov@gmail.com>
2015-05-10 14:52:36 +0300

    Solve image source binding to text box

---
+++ Homework/01-Introduction-to-AngularJS-Homework/03-Bind-Image-Source-to-Text-Box/scripts/controllers/home-controller.js
@@ -0,0 +1,7 @@
+define(['app'], function (app) {
+    app.controller('HomeCtrl', function ($scope) {
+        $scope.picture = {
+            address: 'http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png'
+        };
+    });
+});
