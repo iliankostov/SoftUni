b5ddd96de256846b460565d80989e63bc5f637a9
Ilian Kostov <ilian.kostov@gmail.com>
2015-05-12 16:58:00 +0300

    Add image url validation

--- Homework/01-Introduction-to-AngularJS-Homework/03-Bind-Image-Source-to-Text-Box/scripts/controllers/home-controller.js
+++ Homework/01-Introduction-to-AngularJS-Homework/03-Bind-Image-Source-to-Text-Box/scripts/controllers/home-controller.js
@@ -1,5 +1,6 @@
 define(['app'], function (app) {
     app.controller('HomeCtrl', function ($scope) {
+        $scope.regex = /(https?:\/\/.*\.(?:png|jpg|gif))/i;
         $scope.picture = {
             address: 'http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png'
         };
