058741a22286df101e000b0fe30dc591195d37ab
iliankostov <ilian.kostov@gmail.com>
2015-07-11 23:20:30 +0300

    Solve homework 02 problem 07 Implement LinkedList<T>

---
+++ Homework/02-Linear-Data-Structures-Lists-Homework/LinearDataStructures/07ImplementLinkedList/LinkedList.cs
@@ -0,0 +1,129 @@
+﻿namespace LinearDataStructures
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        public int Count { get; private set; }
+
+        private ListNode<T> Head { get; set; }
+
+        private ListNode<T> Tail { get; set; }
+
+        public void Add(T item)
+        {
+            var newTail = new ListNode<T>(item);
+
+            if (this.Count == 0)
+            {
+                this.Head = this.Tail = newTail;
+            }
+            else
+            {
+                this.Tail.NextNode = newTail;
+                this.Tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public void Remove(uint index)
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("List is empty.");
+            }
+
+            var removedNode = this.ElementAt(index);
+
+            if (removedNode != this.Head)
+            {
+                var previousNode = this.ElementAt(index - 1);
+                previousNode.NextNode = removedNode.NextNode;
+
+                if (removedNode == this.Tail)
+                {
+                    this.Tail = previousNode;
+                }
+            }
+            else
+            {
+                this.Head = this.Head.NextNode;
+            }
+
+            this.Count--;
+        }
+
+        public int FirstIndexOf(T item)
+        {
+            int index = -1,
+                currenntIndex = 0;
+            var currentNode = this.Head;
+
+            while (currenntIndex < this.Count)
+            {
+                if (item.Equals(currentNode.Value))
+                {
+                    index = currenntIndex;
+                    break;
+                }
+
+                currentNode = currentNode.NextNode;
+                currenntIndex++;
+            }
+
+            return index;
+        }
+
+        public int LastIndexOf(T item)
+        {
+            int index = -1,
+                currenntIndex = 0;
+            var currentNode = this.Head;
+
+            while (currenntIndex < this.Count)
+            {
+                if (item.Equals(currentNode.Value))
+                {
+                    index = currenntIndex;
+                }
+
+                currentNode = currentNode.NextNode;
+                currenntIndex++;
+            }
+
+            return index;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = this.Head;
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private ListNode<T> ElementAt(uint index)
+        {
+            var startIndex = 0;
+            var currentNode = this.Head;
+
+            while (startIndex != index)
+            {
+                currentNode = currentNode.NextNode;
+                startIndex++;
+            }
+
+            return currentNode;
+        }
+    }
+}
