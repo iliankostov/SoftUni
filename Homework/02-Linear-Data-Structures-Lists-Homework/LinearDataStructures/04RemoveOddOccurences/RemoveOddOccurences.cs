ea54ca05bb76daad41ca280460803ee560d8ff02
iliankostov <ilian.kostov@gmail.com>
2015-07-11 18:47:55 +0300

    Solve homework 02 problem 04 Remove Odd Ocurences

---
+++ Homework/02-Linear-Data-Structures-Lists-Homework/LinearDataStructures/04RemoveOddOccurences/RemoveOddOccurences.cs
@@ -0,0 +1,36 @@
+﻿namespace LinearDataStructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RemoveOddOccurences
+    {
+        public static void Main()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new NullReferenceException("Sequence cannot be empty!");
+                }
+
+                IList<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+
+                IList<int> sequence = numbers.Where(v => numbers.Count(n => v == n) % 2 == 0).ToList();
+
+                Console.WriteLine(string.Join(" ", sequence));
+            }
+            catch (NullReferenceException nre)
+            {
+                Console.WriteLine(nre.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
