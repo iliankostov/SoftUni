e2b26abd9b3c90aeb252a75761a653e460a4ca10
iliankostov <ilian.kostov@gmail.com>
2015-07-11 21:06:38 +0300

    Try to solve homework 02 problem 07 Implement LinkedList<T>

---
+++ Homework/02-Linear-Data-Structures-Lists-Homework/LinearDataStructures/07ImplementLinkedList/ImplementLinkedList.cs
@@ -0,0 +1,89 @@
+﻿namespace LinearDataStructures
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private class ListNode<T>
+        {
+            public T Value { get; private set; }
+
+            public ListNode<T> NextNode { get; set; }
+
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private ListNode<T> head;
+
+        public int Count { get; private set; }
+
+        public void Add(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = new ListNode<T>(element);
+            }
+            else
+            {
+                var newHead = new ListNode<T>(element);
+                newHead.NextNode = this.head;
+                this.head = newHead;
+            }
+            this.Count++;
+        }
+
+        public void Remove(int index)
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("List empty");
+            }
+
+            this.Count--;
+            throw new NotImplementedException();
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public T[] ToArray()
+        {
+            var arr = new T[this.Count];
+            int index = 0;
+            var currentHead = this.head;
+            while (currentHead != null)
+            {
+                arr[index++] = currentHead.Value;
+                currentHead = currentHead.NextNode;
+            }
+            return arr;
+        }
+    }
+}
