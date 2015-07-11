7dd3e0b14d9b7f154c6f31d77e8b3782fe37dac3
iliankostov <ilian.kostov@gmail.com>
2015-07-11 17:45:25 +0300

    Solve homework 02 problem 01 Sum and Average

---
+++ Homework/02-Linear-Data-Structures-Lists-Homework/LinearDataStructures/LinearDataStructures/SumAndAverage.cs
@@ -0,0 +1,34 @@
+﻿namespace LinearDataStructures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumAndAverage
+    {
+        public static void Main()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new NullReferenceException("Sum=0; Average=0");
+                }
+
+                IList<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+
+                Console.WriteLine("Sum={0}; Average={1}", numbers.Sum(), numbers.Average());
+            }
+            catch (NullReferenceException nre)
+            {
+                Console.WriteLine(nre.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
