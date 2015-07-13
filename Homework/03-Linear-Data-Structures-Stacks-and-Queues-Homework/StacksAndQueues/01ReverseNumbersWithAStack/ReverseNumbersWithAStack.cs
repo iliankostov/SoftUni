06d325e8cedbe4b0ca83aba9ea502c6c20f109dc
iliankostov <ilian.kostov@gmail.com>
2015-07-13 14:28:24 +0300

    Solve homework 03 problem 01 Reverse Numbers With A Stack

---
+++ Homework/03-Linear-Data-Structures-Stacks-and-Queues-Homework/StacksAndQueues/01ReverseNumbersWithAStack/ReverseNumbersWithAStack.cs
@@ -0,0 +1,41 @@
+﻿namespace StacksAndQueues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ReverseNumbersWithAStack
+    {
+        public static void Main()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(empty)")
+                {
+                    throw new NullReferenceException("(empty)");
+                }
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new NullReferenceException("(empty)");
+                }
+
+                Stack<int> numbers = new Stack<int>();
+
+                input.Split(' ').Select(int.Parse).ToList().ForEach(numbers.Push);
+
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+            catch (NullReferenceException nre)
+            {
+                Console.WriteLine(nre.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
