7079b06d4ed08a39ff75ded90b67608071626c51
iliankostov <ilian.kostov@gmail.com>
2015-07-15 13:22:38 +0300

    Solve homework 01 problem 02 Employee DAO Class

---
+++ Homework/01-Entity-Framework-Homework/EntityFramework/02EmployeeDAOClass/EmployeeDAOTest.cs
@@ -0,0 +1,31 @@
+﻿namespace EntityFramework
+{
+    using System;
+
+    public class EmployeeDAOTest
+    {
+        public static void Main()
+        {
+            var ivan = new Employee
+                              {
+                                  FirstName = "Ivan",
+                                  LastName = "Ivanov",
+                                  MiddleName = "I",
+                                  JobTitle = "Production Technician",
+                                  DepartmentID = 7,
+                                  HireDate = DateTime.Now,
+                                  Salary = 30000
+                              };
+
+            EmployeeDAO.Add(ivan);
+            Console.WriteLine("Successfully add {0} with ID = {1}", ivan.FirstName, ivan.EmployeeID);
+
+            ivan.FirstName = "Ivancho";
+            EmployeeDAO.Modify(ivan);
+            Console.WriteLine("Successfully change first name to {0}", ivan.FirstName);
+
+            EmployeeDAO.Delete(ivan);
+            Console.WriteLine("Successfully delete {0} with Id = {1}", ivan.FirstName, ivan.EmployeeID);
+        }
+    }
+}
