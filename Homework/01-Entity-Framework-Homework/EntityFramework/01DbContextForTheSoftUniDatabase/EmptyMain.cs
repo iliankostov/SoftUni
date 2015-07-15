b170045f24536615eef07aec5713f4290b12d1ef
iliankostov <ilian.kostov@gmail.com>
2015-07-15 13:21:46 +0300

    Solve homework 01 problem 01 DbContext for the SoftUni Database

---
+++ Homework/01-Entity-Framework-Homework/EntityFramework/01DbContextForTheSoftUniDatabase/EmptyMain.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityFramework
+{
+    using System;
+
+    public class EmptyMain
+    {
+        public static void Main()
+        {
+            //// Find the solutions at next projects.
+        }
+    }
+}
