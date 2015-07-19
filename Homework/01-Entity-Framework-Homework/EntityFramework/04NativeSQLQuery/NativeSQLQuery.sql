d1e1fa8c74fcde4832b330b9b23198d6a7378fa9
iliankostov <ilian.kostov@gmail.com>
2015-07-19 22:32:05 +0300

    Try to solve homework 01 problem 04 Native SQL query

---
+++ Homework/01-Entity-Framework-Homework/EntityFramework/04NativeSQLQuery/NativeSQLQuery.sql
@@ -0,0 +1,8 @@
+﻿USE SoftUni
+GO
+
+SELECT Count(*) 
+FROM Employees AS e 
+JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p ON p.ProjectID = ep.ProjectID
+WHERE YEAR(p.StartDate) = '2002'
