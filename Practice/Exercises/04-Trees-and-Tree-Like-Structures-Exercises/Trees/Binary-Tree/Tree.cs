93b252159edafa4b66d444b68b495176738495ab
iliankostov <ilian.kostov@gmail.com>
2015-07-24 20:28:33 +0300

    Implement Tree with Print() and Each() methods

--- Practice/Exercises/04-Trees-and-Tree-Like-Structures-Exercises/Trees/Binary-Tree/Tree.cs
+++ Practice/Exercises/04-Trees-and-Tree-Like-Structures-Exercises/Trees/Binary-Tree/Tree.cs
@@ -5,16 +5,34 @@ public class Tree<T>
 {
     public Tree(T value, params Tree<T>[] children)
     {
-      // TODO
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+        }
     }
+
+    public T Value { get; set; }
+
+    public IList<Tree<T>> Children { get; set; }
 
     public void Print(int indent = 0)
     {
-        // TODO
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.Value);
+        foreach (var child in this.Children)
+        {
+            child.Print(indent + 1);
+        }
     }
 
     public void Each(Action<T> action)
     {
-        // TODO
+        action(this.Value);
+        foreach (var child in this.Children)
+        {
+            child.Each(action);
+        }
     }
 }
