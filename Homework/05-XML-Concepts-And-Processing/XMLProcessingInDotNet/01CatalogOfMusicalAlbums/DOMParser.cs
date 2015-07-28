a3fd73acb488d132fc7c844b0e5d4177ee639ad9
iliankostov <ilian.kostov@gmail.com>
2015-07-28 15:37:30 +0300

    Solve homework 05 part 2 problems 01 and 02 Catalog of musical albums

---
+++ Homework/05-XML-Concepts-And-Processing/XMLProcessingInDotNet/01CatalogOfMusicalAlbums/DOMParser.cs
@@ -0,0 +1,40 @@
+﻿namespace XMLProcessingInDotNet
+{
+    using System;
+    using System.Xml;
+
+    public class DOMParser
+    {
+        public static void Main()
+        {
+            Console.Write("Please choise number of problem in interval [2..10]: ");
+            var input = Console.ReadLine();
+            XmlDocument catalog = new XmlDocument();
+            catalog.Load(@"..\..\catalog.xml");
+            XmlNode rootNode = catalog.DocumentElement;
+            if (rootNode != null)
+            {
+                switch (input)
+                {
+                    case "2":
+                        foreach (XmlNode childNodes in rootNode.ChildNodes)
+                        {
+                            foreach (XmlNode childNode in childNodes)
+                            {
+                                if (childNode.Name == "name")
+                                {
+                                    Console.WriteLine(childNode.InnerText);
+                                }
+                            }
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong input.");
+                        break;
+                }
+            }
+        }
+    }
+}
