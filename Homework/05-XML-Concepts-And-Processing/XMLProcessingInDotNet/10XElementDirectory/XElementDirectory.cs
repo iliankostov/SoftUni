ed0722ecd30d9b6b29250a383071bbcf0ea0032f
iliankostov <ilian.kostov@gmail.com>
2015-07-29 21:04:48 +0300

    Solve homework 05 problem 10 XElement Directory Contents as XML

---
+++ Homework/05-XML-Concepts-And-Processing/XMLProcessingInDotNet/10XElementDirectory/XElementDirectory.cs
@@ -0,0 +1,46 @@
+﻿namespace XMLProcessingInDotNet
+{
+    using System.IO;
+    using System.Xml.Linq;
+
+    public class XElementDirectory
+    {
+        private static XDocument xDocument = new XDocument();
+
+        private static string folderPath = @"C:\Media\";
+
+        private static bool isRoot = true;
+
+        public static void Main()
+        {
+            var folder = new Folder(folderPath);
+            xDocument.Add(RecursiveDirectoryTraversal(folder));
+            xDocument.Save("../../XElementDirectory.xml");
+        }
+
+        private static XElement RecursiveDirectoryTraversal(Folder folder)
+        {
+            var directoryInfo = new DirectoryInfo(folder.Name);
+
+            var dirElement = new XElement(isRoot ? "root-dir" : "dir", new XAttribute("name", folder.Name));
+            isRoot = false;
+
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                folder.Files.Add(new File(file.FullName));
+                var fileElement = new XElement("file", new XAttribute("name", file.Name));
+                dirElement.Add(fileElement);
+            }
+
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                var childFolder = new Folder(directory.FullName);
+                folder.ChildFolders.Add(childFolder);
+
+                dirElement.Add(RecursiveDirectoryTraversal(childFolder));
+            }
+
+            return dirElement;
+        }
+    }
+}
