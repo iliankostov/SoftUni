1df4baa48adf226ee433b8c26b002fbe02523519
iliankostov <ilian.kostov@gmail.com>
2015-07-29 14:43:10 +0300

    Solve homework 05 problems 03 and 04 DOM Parser

--- Homework/05-XML-Concepts-And-Processing/XMLProcessingInDotNet/01CatalogOfMusicalAlbums/DOMParser.cs
+++ Homework/05-XML-Concepts-And-Processing/XMLProcessingInDotNet/01CatalogOfMusicalAlbums/DOMParser.cs
@@ -1,6 +1,7 @@
 ﻿namespace XMLProcessingInDotNet
 {
     using System;
+    using System.Collections.Generic;
     using System.Xml;
 
     public class DOMParser
@@ -29,12 +30,56 @@
                         }
 
                         break;
+                    case "3":
+                        var sortedArtists = new SortedSet<string>();
+                        foreach (XmlNode childNodes in rootNode.ChildNodes)
+                        {
+                            foreach (XmlNode childNode in childNodes)
+                            {
+                                if (childNode.Name == "artist")
+                                {
+                                    sortedArtists.Add(childNode.InnerText);
+                                }
+                            }
+                        }
 
+                        Console.WriteLine(string.Join(Environment.NewLine, sortedArtists));
+                        break;
+                    case "4":
+                        var uniqueArtists = new Dictionary<string, int>();
+                        foreach (XmlNode childNodes in rootNode.ChildNodes)
+                        {
+                            foreach (XmlNode childNode in childNodes)
+                            {
+                                if (childNode.Name == "artist")
+                                {
+                                    var key = childNode.InnerText;
+
+                                    if (uniqueArtists.ContainsKey(key))
+                                    {
+                                        uniqueArtists[key]++;
+                                    }
+                                    else
+                                    {
+                                        uniqueArtists[key] = 1;
+                                    }
+                                }
+                            }
+                        }
+
+                        foreach (var uniqueArtist in uniqueArtists)
+                        {
+                            Console.WriteLine(uniqueArtist.Key + " " + uniqueArtist.Value);
+                        }
+
+                        break;
                     default:
                         Console.WriteLine("Wrong input.");
                         break;
                 }
             }
         }
+
+
     }
 }
