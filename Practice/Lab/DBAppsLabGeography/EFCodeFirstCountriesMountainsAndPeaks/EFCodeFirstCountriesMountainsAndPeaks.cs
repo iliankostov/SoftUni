a00b252f713f86558e3c4f4be04fe6f40af37e0a
iliankostov <ilian.kostov@gmail.com>
2015-07-30 21:39:25 +0300

    Solve DB Apps Lab Geography

---
+++ Practice/Lab/DBAppsLabGeography/EFCodeFirstCountriesMountainsAndPeaks/EFCodeFirstCountriesMountainsAndPeaks.cs
@@ -0,0 +1,37 @@
+﻿namespace EFCodeFirstCountriesMountainsAndPeaks
+{
+    using System;
+    using System.Linq;
+
+    public class EFCodeFirstCountriesMountainsAndPeaks
+    {
+        public static void Main()
+        {
+            var context = new GeographyEntities();
+
+            var countryQueries = context.Countries.Select(
+                c => new
+                         {
+                             CountryName = c.CountryName,
+                             Mountains = c.Mountains.Select(
+                                 m => new
+                                          {
+                                              m.Name,
+                                              m.Peaks
+                                          })
+                         });
+            foreach (var country in countryQueries)
+            {
+                Console.WriteLine("Country: " + country.CountryName);
+                foreach (var mountain in country.Mountains)
+                {
+                    Console.WriteLine(" Mountain: " + mountain.Name);
+                    foreach (var peak in mountain.Peaks)
+                    {
+                        Console.WriteLine("\t{0} ({1})", peak.Name, peak.Elevation);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Practice/Lab/DBAppsLabGeography/ExportRiversAsJSON/ExportRiversAsJSON.cs
@@ -0,0 +1,34 @@
+﻿namespace ExportRiversAsJSON
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Web.Script.Serialization;
+
+    using EFMapping;
+
+    public class ExportRiversAsJSON
+    {
+        public static void Main()
+        {
+            var context = new GeographyEntities();
+
+            var rivers = context.Rivers
+                .OrderByDescending(r => r.Length)
+                .Select(r => new
+                             {
+                                 riverName = r.RiverName,
+                                 riverLenght = r.Length,
+                                 countries = r.Countries
+                                     .OrderBy(c => c.CountryName)
+                                     .Select(c => c.CountryName)
+                             });
+
+            var js = new JavaScriptSerializer();
+            var riverJson = js.Serialize(rivers.ToList());
+            
+            File.WriteAllText(@"..\..\rivers.json", riverJson);
+            Console.WriteLine("Export rivers.json complited!");
+        }
+    }
+}
