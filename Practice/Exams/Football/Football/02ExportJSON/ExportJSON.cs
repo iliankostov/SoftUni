3aa4b353b4ffa4281ac282338b9ad41247c6daf3
iliankostov <ilian.kostov@gmail.com>
2015-08-01 22:32:55 +0300

    Practice Exams Football

---
+++ Practice/Exams/Football/Football/02ExportJSON/ExportJSON.cs
@@ -0,0 +1,30 @@
+﻿namespace Football
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    public class ExportJSON
+    {
+        public static void Main()
+        {
+            var context = new FootballEntities();
+
+            var leagues = context.Leagues
+                .Select(l => new
+                             {
+                                 leagueName = l.LeagueName,
+                                 teams = l.Teams
+                                    .OrderBy(t => t.TeamName)
+                                    .Select(t => t.TeamName)
+                             })
+                .OrderBy(l => l.leagueName);
+
+            var jsonString = JsonConvert.SerializeObject(leagues);
+            File.AppendAllText(@"..\..\leagues-and-teams.json", jsonString);
+            Console.WriteLine("Export to leagues-and-teams.json completed!");
+        }
+    }
+}
