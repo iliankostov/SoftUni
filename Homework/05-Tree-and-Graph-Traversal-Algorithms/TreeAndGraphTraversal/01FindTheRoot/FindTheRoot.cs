bd9bdb7f04f356facac8812b6aaef4ad1965e390
iliankostov <ilian.kostov@gmail.com>
2015-08-02 17:37:24 +0300

    Solve homework 05 problem 01 Find The Root

---
+++ Homework/05-Tree-and-Graph-Traversal-Algorithms/TreeAndGraphTraversal/01FindTheRoot/FindTheRoot.cs
@@ -0,0 +1,44 @@
+﻿namespace TreeAndGraphTraversal
+{
+    using System;
+
+    public class FindTheRoot
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool[] hasParent = new bool[n + 1];
+            int count = 0;
+            int root = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var nums = Console.ReadLine().Split(' ');
+                var toNode = int.Parse(nums[1]);
+                hasParent[toNode] = true;
+            }
+
+            for (int i = 0; i < hasParent.Length; i++)
+            {
+                if (!hasParent[i])
+                {
+                    count++;
+                    root = i;
+                }
+            }
+
+            if (count == 1)
+            {
+                Console.WriteLine("Root = " + root);
+            }
+            else if (count > 1)
+            {
+                Console.WriteLine("Forest is not a tree!");
+            }
+            else
+            {
+                Console.WriteLine("No root!");
+            }
+        }
+    }
+}
