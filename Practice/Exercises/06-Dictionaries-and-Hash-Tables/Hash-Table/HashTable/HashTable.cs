ed74334378e501d5ed2f4aae0bb3204524d403f0
iliankostov <ilian.kostov@gmail.com>
2015-08-05 13:50:16 +0300

    Solve Exercise 06 Hash-Table

---
+++ Practice/Exercises/06-Dictionaries-and-Hash-Tables/Hash-Table/HashTable/HashTable.cs
@@ -0,0 +1,266 @@
+﻿namespace HashTable
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
+    {
+        private const int DefaultCapacity = 16;
+
+        private const float LoadFactor = 0.75f;
+
+        private LinkedList<KeyValue<TKey, TValue>>[] slots;
+
+        public HashTable(int capacity = DefaultCapacity)
+        {
+            this.slots = new LinkedList<KeyValue<TKey, TValue>>[capacity];
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.slots.Length;
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            this.GrowIfNeeded();
+            int slotNumber = this.FindSlotNumber(key);
+            if (this.slots[slotNumber] == null)
+            {
+                this.slots[slotNumber] = new LinkedList<KeyValue<TKey, TValue>>();
+            }
+
+            foreach (var element in this.slots[slotNumber])
+            {
+                if (element.Key.Equals(key))
+                {
+                    throw new ArgumentException("Key already exists: " + key);
+                }
+            }
+
+            var newElement = new KeyValue<TKey, TValue>(key, value);
+            this.slots[slotNumber].AddLast(newElement);
+            this.Count++;
+        }
+
+        private int FindSlotNumber(TKey key)
+        {
+            return Math.Abs(key.GetHashCode()) % this.slots.Length;
+        }
+
+        public bool AddOrReplace(TKey key, TValue value)
+        {
+            this.GrowIfNeeded();
+            int slotNumber = this.FindSlotNumber(key);
+            if (this.slots[slotNumber] == null)
+            {
+                this.slots[slotNumber] = new LinkedList<KeyValue<TKey, TValue>>();
+            }
+
+            foreach (var element in this.slots[slotNumber])
+            {
+                if (element.Key.Equals(key))
+                {
+                    element.Value = value;
+                    return false;
+                }
+            }
+
+            var newElement = new KeyValue<TKey, TValue>(key, value);
+            this.slots[slotNumber].AddLast(newElement);
+            this.Count++;
+            return true;
+        }
+
+        private void Grow()
+        {
+            var newHashTable = new HashTable<TKey, TValue>(2 * this.Capacity);
+
+            foreach (var element in this)
+            {
+                newHashTable.Add(element.Key, element.Value);
+            }
+
+            this.slots = newHashTable.slots;
+            this.Count = newHashTable.Count;
+        }
+
+        private void GrowIfNeeded()
+        {
+            if ((float)(this.Count + 1) / this.Capacity > LoadFactor)
+            {
+                this.Grow();
+            }
+        }
+
+        public TValue Get(TKey key)
+        {
+            var element = this.Find(key);
+            if (element == null)
+            {
+                throw new KeyNotFoundException("Key not fount: " + key);
+            }
+
+            return element.Value;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return this.Get(key);
+            }
+
+            set
+            {
+                this.AddOrReplace(key, value);
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var element = this.Find(key);
+            if (element == null)
+            {
+                value = default(TValue);
+                return false;
+            }
+
+            value = element.Value;
+            return true;
+        }
+
+        public KeyValue<TKey, TValue> Find(TKey key)
+        {
+            int slotNumber = this.FindSlotNumber(key);
+            var elements = this.slots[slotNumber];
+            if (elements != null)
+            {
+                foreach (var element in elements)
+                {
+                    if (element.Key.Equals(key))
+                    {
+                        return element;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            var element = this.Find(key);
+            return element != null;
+        }
+
+        public bool Remove(TKey key)
+        {
+            int slotNumber = this.FindSlotNumber(key);
+            var elements = this.slots[slotNumber];
+            if (elements != null)
+            {
+                foreach (var element in elements)
+                {
+                    if (element.Key.Equals(key))
+                    {
+                        elements.Remove(element);
+                        this.Count--;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            this.slots = new LinkedList<KeyValue<TKey, TValue>>[DefaultCapacity];
+            this.Count = 0;
+        }
+
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                return this.Select(e => e.Key);
+            }
+        }
+
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                return this.Select(e => e.Value);
+            }
+        }
+
+        public IEnumerator<KeyValue<TKey, TValue>> GetEnumerator()
+        {
+            foreach (var elements in this.slots)
+            {
+                if (elements != null)
+                {
+                    foreach (var element in elements)
+                    {
+                        yield return element;
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int CalculateCollisionsDeep()
+        {
+            int count = 0;
+
+            if (this.slots != null)
+            {
+                count = this.slots
+                    .Where(elements => elements != null)
+                    .Max(elements => elements
+                        .Count(element => elements.Count > 1)) - 1;
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+
+            if (this.slots != null)
+            {
+                for (int i = 0; i < this.slots.Length; i++)
+                {
+                    var elements = this.slots[i];
+                    if (elements != null)
+                    {
+                        output.Append(string.Format("{0}: ", i));
+                        output.AppendLine(string.Join(" ", elements));
+                    }
+                    else
+                    {
+                        output.AppendLine(string.Format("{0}: ", i));
+                    }
+                }
+            }
+
+            return output.ToString();
+        }
+    }
+}
