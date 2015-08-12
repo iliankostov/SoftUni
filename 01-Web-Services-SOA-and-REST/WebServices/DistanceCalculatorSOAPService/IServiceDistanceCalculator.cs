18345c59cd86c66fb9e7c5921e816fca15dcad57
iliankostov <ilian.kostov@gmail.com>
2015-08-12 23:42:29 +0300

    Solve homework 01 problem 01 Distance Calculator SOAP Service

---
+++ 01-Web-Services-SOA-and-REST/WebServices/DistanceCalculatorSOAPService/IServiceDistanceCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace DistanceCalculatorSOAPService
+{
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface IServiceDistanceCalculator
+    {
+        [OperationContract]
+        double CalculateDistance(int startX, int startY, int endX, int endY);
+    }
+}
---
+++ 01-Web-Services-SOA-and-REST/WebServices/DistanceCalculatorSOAPService/ServiceDistanceCalculator.svc.cs
@@ -0,0 +1,12 @@
+﻿namespace DistanceCalculatorSOAPService
+{
+    using System;
+
+    public class ServiceDistanceCalculator : IServiceDistanceCalculator
+    {
+        public double CalculateDistance(int startX, int startY, int endX, int endY)
+        {
+            return Math.Sqrt(Math.Pow(startX - endX, 2) + Math.Pow(startY - endY, 2));
+        }
+    }
+}
