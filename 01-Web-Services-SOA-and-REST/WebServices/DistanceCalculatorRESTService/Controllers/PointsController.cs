3a282277de969c7ecb668b278cc79b0a143a5273
iliankostov <ilian.kostov@gmail.com>
2015-08-13 15:16:22 +0300

    Solve homework 01 problem 03 Distance Calculator REST Service

---
+++ 01-Web-Services-SOA-and-REST/WebServices/DistanceCalculatorRESTService/Controllers/PointsController.cs
@@ -0,0 +1,13 @@
+﻿namespace DistanceCalculatorRESTService.Controllers
+{
+    using System;
+    using System.Web.Http;
+
+    public class PointsController : ApiController
+    {
+        public double Get(int startX, int startY, int endX, int endY)
+        {
+            return Math.Sqrt(Math.Pow(startX - endX, 2) + Math.Pow(startY - endY, 2));
+        }
+    }
+}
