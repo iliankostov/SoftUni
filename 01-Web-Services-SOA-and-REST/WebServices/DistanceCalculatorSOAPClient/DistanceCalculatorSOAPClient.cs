362054eeb4aaf6507b90e57392448ef7bf486624
iliankostov <ilian.kostov@gmail.com>
2015-08-13 12:58:11 +0300

    Solve homework 01 problem 02 Distance Calculator SOAP Client

---
+++ 01-Web-Services-SOA-and-REST/WebServices/DistanceCalculatorSOAPClient/DistanceCalculatorSOAPClient.cs
@@ -0,0 +1,16 @@
+﻿namespace DistanceCalculatorSOAPClient
+{
+    using System;
+
+    using ServiceReferenceCalculator;
+
+    internal class DistanceCalculatorSOAPClient
+    {
+        private static void Main()
+        {
+            var client = new ServiceDistanceCalculatorClient();
+            var result = client.CalculateDistance(0, 5, 10, 5);
+            Console.WriteLine(result);
+        }
+    }
+}
