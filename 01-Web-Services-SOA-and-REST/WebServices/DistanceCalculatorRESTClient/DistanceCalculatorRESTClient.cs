3940c6197b588db6de1f933ac07d3bdce5f8986c
iliankostov <ilian.kostov@gmail.com>
2015-08-13 15:57:23 +0300

    Solve homework 01 problem 04 Distance Calculator REST Client

---
+++ 01-Web-Services-SOA-and-REST/WebServices/DistanceCalculatorRESTClient/DistanceCalculatorRESTClient.cs
@@ -0,0 +1,22 @@
+﻿namespace DistanceCalculatorRESTClient
+{
+    using System;
+
+    using RestSharp;
+
+    internal class DistanceCalculatorRESTClient
+    {
+        private static void Main()
+        {
+            var client = new RestClient("http://localhost:2038/api/");
+            var request = new RestRequest("points", Method.GET);
+            request.AddParameter("startX", 5);
+            request.AddParameter("startY", 5);
+            request.AddParameter("endX", 10);
+            request.AddParameter("endY", 10);
+
+            var response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
