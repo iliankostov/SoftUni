b49a6eac235802a5a1cba6cfc413140869b41db9
iliankostov <ilian.kostov@gmail.com>
2015-08-17 20:20:42 +0300

    Solve homework 07 problem 03 Implement a Binary Heap

---
+++ Homework/07-Collection-Data-Structures-and-Libraries/Collections/TestBinaryHeap/TestBinaryHeap.cs
@@ -0,0 +1,97 @@
+﻿namespace TestBinaryHeap
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using ImplementBinaryHeap;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestBinaryHeap
+    {
+        [TestMethod]
+        public void TestAddAndCount()
+        {
+            var minHeap = new MinHeap<long>();
+            minHeap.Add(9999999999);
+            Assert.AreEqual(1, minHeap.Count);
+        }
+
+        [TestMethod]
+        public void TestExtractAndCount()
+        {
+            var maxHeap = new MaxHeap<string>();
+            maxHeap.Add("Ivan");
+            maxHeap.Add("George");
+            var expected = "Pesho";
+            maxHeap.Add(expected);
+            var actual = maxHeap.ExtractDominating();
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(2, maxHeap.Count);
+        }
+
+        [TestMethod]
+        public void TestHeapBySortingInts()
+        {
+            var minHeap = new MinHeap<int>(new[] { 9, 8, 4, 1, 6, 2, 7, 4, 1, 2 });
+            AssertHeapSortInts(minHeap, minHeap.OrderBy(i => i).ToArray());
+
+            minHeap = new MinHeap<int> { 7, 5, 1, 6, 3, 2, 4, 1, 2, 1, 3, 4, 7 };
+            AssertHeapSortInts(minHeap, minHeap.OrderBy(i => i).ToArray());
+
+            var maxHeap = new MaxHeap<int>(new[] { 1, 5, 3, 2, 7, 56, 3, 1, 23, 5, 2, 1 });
+            AssertHeapSortInts(maxHeap, maxHeap.OrderBy(d => -d).ToArray());
+
+            maxHeap = new MaxHeap<int> { 2, 6, 1, 3, 56, 1, 4, 7, 8, 23, 4, 5, 7, 34, 1, 4 };
+            AssertHeapSortInts(maxHeap, maxHeap.OrderBy(d => -d).ToArray());
+        }
+
+        [TestMethod]
+        public void TestHeapBySortingStrings()
+        {
+            var minHeap = new MinHeap<string>(new[] { "do", "re", "mi", "fa", "sol", "la", "si", "do" });
+            AssertHeapSortStrings(minHeap, minHeap.OrderBy(i => i).ToArray());
+
+            minHeap = new MinHeap<string> { "a", "d", "b", "e", "c", "k", "o", "l", "p", "m", "q", "n", "r" };
+            AssertHeapSortStrings(minHeap, minHeap.OrderBy(i => i).ToArray());
+        }
+
+        [TestMethod]
+        public void TestHeapBySortingObjects()
+        {
+            var productOne = new TestProduct("ProductOne", 100M);
+            var productTwo = new TestProduct("ProductTwo", 1000M);
+            var productThree = new TestProduct("ProductOne", 20M);
+            var productSame = new TestProduct("ProductOne", 20M);
+            var productFour = new TestProduct("ProductOne", 200M);
+            var productFive = new TestProduct("ProductOne", 500M);
+
+            var minHeap = new MinHeap<TestProduct> { productOne, productTwo, productThree, productSame, productFour, productFive };
+            var actual = minHeap.ExtractDominating();
+            Assert.AreEqual(productThree, actual);
+        }
+
+        private static void AssertHeapSortInts(Heap<int> heap, IEnumerable<int> expected)
+        {
+            var sorted = new List<int>();
+            while (heap.Count > 0)
+            {
+                sorted.Add(heap.ExtractDominating());
+            }
+
+            Assert.IsTrue(sorted.SequenceEqual(expected));
+        }
+
+        private static void AssertHeapSortStrings(Heap<string> heap, IEnumerable<string> expected)
+        {
+            var sorted = new List<string>();
+            while (heap.Count > 0)
+            {
+                sorted.Add(heap.ExtractDominating());
+            }
+
+            Assert.IsTrue(sorted.SequenceEqual(expected));
+        }
+    }
+}
---
+++ Homework/07-Collection-Data-Structures-and-Libraries/Collections/TestBinaryHeap/TestProduct.cs
@@ -0,0 +1,34 @@
+﻿namespace TestBinaryHeap
+{
+    using System;
+
+    internal class TestProduct : IComparable
+    {
+        public TestProduct(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var product = (TestProduct)obj;
+
+            if (this.Price.CompareTo(product.Price) > 0)
+            {
+                return 1;
+            }
+
+            if (this.Price.CompareTo(product.Price) < 0)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
