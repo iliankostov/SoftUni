7bcdbee0bb4979f66e628baf7159adb1eaefc247
iliankostov <ilian.kostov@gmail.com>
2015-08-17 20:46:55 +0300

    Solve homework 07 problem 04 String Editor

---
+++ Homework/07-Collection-Data-Structures-and-Libraries/Collections/StringEditor/StringEditor.cs
@@ -0,0 +1,53 @@
+﻿namespace StringEditor
+{
+    using System.Linq;
+
+    using Wintellect.PowerCollections;
+
+    internal class StringEditor
+    {
+        private readonly BigList<char> rope;
+
+        public StringEditor()
+        {
+            this.rope = new BigList<char>();
+        }
+
+        public void Insert(string[] input)
+        {
+            var chars = input[1].Select(ch => ch);
+            var startIndex = int.Parse(input[2]);
+
+            this.rope.InsertRange(startIndex, chars);
+        }
+
+        public void Append(string[] input)
+        {
+            var chars = input[1].Select(ch => ch);
+            this.rope.AddRange(chars);
+        }
+
+        public void Delete(string[] input)
+        {
+            var startIndex = int.Parse(input[1]);
+            var count = int.Parse(input[2]);
+
+            this.rope.RemoveRange(startIndex, count);
+        }
+
+        public void Replace(string[] input)
+        {
+            var startIndex = int.Parse(input[1]);
+            var count = int.Parse(input[2]);
+            var chars = input[3].Select(ch => ch);
+
+            this.rope.RemoveRange(startIndex, count);
+            this.rope.InsertRange(startIndex, chars);
+        }
+
+        public string PrintString()
+        {
+            return string.Join(string.Empty, this.rope);
+        }
+    }
+}
---
+++ Homework/07-Collection-Data-Structures-and-Libraries/Collections/StringEditor/StringEditorTest.cs
@@ -0,0 +1,50 @@
+﻿namespace StringEditor
+{
+    using System;
+
+    internal class StringEditorTest
+    {
+        private static void Main()
+        {
+            var stringEditor = new StringEditor();
+            string command = string.Empty;
+            while (command != "END")
+            {
+                try
+                {
+                    var input = Console.ReadLine().Split(' ');
+                    command = input[0].ToUpper();
+
+                    switch (command)
+                    {
+                        case "INSERT":
+                            stringEditor.Insert(input);
+                            break;
+                        case "APPEND":
+                            stringEditor.Append(input);
+                            break;
+                        case "DELETE":
+                            stringEditor.Delete(input);
+                            break;
+                        case "REPLACE":
+                            stringEditor.Replace(input);
+                            break;
+                        case "PRINT":
+                            Console.WriteLine(stringEditor.PrintString());
+                            break;
+                        case "END":
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException("INVALID" + " COMMAND");
+                    }
+
+                    Console.WriteLine("OK");
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine("ERROR " + ex.ParamName);
+                }
+            }
+        }
+    }
+}
