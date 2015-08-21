23a4834526a743f6a6a8de07dcb3a2d7eb6f8979
iliankostov <ilian.kostov@gmail.com>
2015-08-21 17:26:09 +0300

    Configure app start settings

--- Homework/02-ASP.NET-Web-API/ASP.NETWebAPI/Service/App_Start/FilterConfig.cs
+++ Homework/02-ASP.NET-Web-API/ASP.NETWebAPI/Service/App_Start/FilterConfig.cs
@@ -1,8 +1,7 @@
-﻿using System.Web;
-using System.Web.Mvc;
-
-namespace Service
+﻿namespace Service
 {
+    using System.Web.Mvc;
+
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
--- Homework/02-ASP.NET-Web-API/ASP.NETWebAPI/Service/App_Start/WebApiConfig.cs
+++ Homework/02-ASP.NET-Web-API/ASP.NETWebAPI/Service/App_Start/WebApiConfig.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
-
-namespace Service
+﻿namespace Service
 {
+    using System.Web.Http;
+
+    using Microsoft.Owin.Security.OAuth;
+
+    using Newtonsoft.Json.Serialization;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -20,11 +18,10 @@ namespace Service
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
         }
     }
 }
