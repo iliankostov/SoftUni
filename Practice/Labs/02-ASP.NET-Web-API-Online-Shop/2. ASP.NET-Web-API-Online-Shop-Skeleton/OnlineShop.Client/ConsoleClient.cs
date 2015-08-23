9bad9997759573d8483d5c29bc7cac6931154613
iliankostov <ilian.kostov@gmail.com>
2015-08-23 16:44:01 +0300

    Solve exercice 02 problem 01 Define the Data Layer

---
+++ Practice/Labs/02-ASP.NET-Web-API-Online-Shop/2. ASP.NET-Web-API-Online-Shop-Skeleton/OnlineShop.Client/ConsoleClient.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineShop.Client
+{
+    using System;
+    using System.Linq;
+    using OnlineShop.Data;
+
+    internal class ConsoleClient
+    {
+        private static void Main()
+        {
+            OnlineShopContext context = new OnlineShopContext();
+            Console.WriteLine(context.Categories.Count());
+        }
+    }
+}
