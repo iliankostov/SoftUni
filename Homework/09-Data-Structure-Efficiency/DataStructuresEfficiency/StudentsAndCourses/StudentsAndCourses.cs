00ca0dded44c9a9999cde03a8600d11766b9e599
iliankostov <ilian.kostov@gmail.com>
2015-09-06 17:49:52 +0300

    Solve homework 09 problem 01 Students and Courses

---
+++ Homework/09-Data-Structure-Efficiency/DataStructuresEfficiency/StudentsAndCourses/StudentsAndCourses.cs
@@ -0,0 +1,39 @@
+﻿namespace StudentsAndCourses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    internal class StudentsAndCourses
+    {
+        private static void Main()
+        {
+            var personsByCourses = new SortedDictionary<string, SortedSet<Person>>();
+
+            var file = new StreamReader("../../input.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                var personData = line.Split('|');
+                var firstName = personData[0].Trim();
+                var lastName = personData[1].Trim();
+                var course = personData[2].Trim();
+
+                var person = new Person
+                    {
+                        FirstName = firstName, 
+                        LastName = lastName
+                    };
+
+                personsByCourses.EnsureKeyExists(course);
+                personsByCourses[course].Add(person);
+            }
+
+            foreach (var personsByCourse in personsByCourses)
+            {
+                Console.Write(personsByCourse.Key + ": ");
+                Console.WriteLine(string.Join(", ", personsByCourse.Value));
+            }
+        }
+    }
+}
