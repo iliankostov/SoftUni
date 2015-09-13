88e1da9dc522168252e7db8bbc73ca1e0cd2186b
iliankostov <ilian.kostov@gmail.com>
2015-09-13 16:51:14 +0300

    Solve Exam 13 September 2015

---
+++ Practice/Exams/Exam-Data-Structures-13-September-2015/First-Last-List/C#/First-Last-List/FirstLastList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T>
+    where T : IComparable<T>
+{
+    private Deque<T> elements = new Deque<T>();
+
+    public void Add(T newElement)
+    {
+        this.elements.Add(newElement);
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.elements.Count;
+        }
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.Take(count);
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.Skip(Math.Max(0, this.elements.Count() - count)).Reverse();
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.OrderBy(e => e).Take(count);
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.OrderByDescending(e => e).Take(count);
+    }
+
+    public int RemoveAll(T element)
+    {
+        var result = this.elements.RemoveAll(e => e.CompareTo(element) == 0);
+
+        if (result == null)
+        {
+            return 0;
+        }
+
+        return result.Count;
+    }
+
+    public void Clear()
+    {
+        this.elements.Clear();
+    }
+}
