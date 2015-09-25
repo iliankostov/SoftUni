5e8390d14da482d64e345b92bb19574a62ff3c2a
iliankostov <ilian.kostov@gmail.com>
2015-09-25 14:19:59 +0300

    database script

---
+++ Practice/05-MVC-Concepts-Exercise/application.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS `mvc_app`;
+
+CREATE DATABASE `mvc_app`
+  CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+
+USE `mvc_app`;
+
+CREATE TABLE `users` (
+  id           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username     VARCHAR(255),
+  passwordHash VARCHAR(255),
+  gold         INT(11),
+  food         INT(11)
+);
+
+CREATE TABLE `buildings` (
+  id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE `building_levels` (
+  building_id INT(11),
+  level       INT(11),
+  gold        INT(11),
+  food        INT(11),
+  FOREIGN KEY (building_id) REFERENCES buildings (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE `user_buildings_level` (
+  user_id     INT(11),
+  building_id INT(11),
+  level       INT(11),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (building_id) REFERENCES buildings (id)
+    ON DELETE CASCADE,
+  UNIQUE (user_id, building_id, level)
+);
