335a8b8262747b345ddee0e49b2bca249dab5eab
iliankostov <ilian.kostov@gmail.com>
2015-09-27 18:21:41 +0300

    Solve homework 01 problem 02 Nested Loop To Recursion

---
+++ Homework/01Recursion/Recursion/NestedLoopsToRecursion/NestedLoopsToRecursion.cs
@@ -0,0 +1,38 @@
+﻿namespace NestedLoopsToRecursion
+{
+    using System;
+
+    internal class NestedLoopsToRecursion
+    {
+        private static int[] arr;
+
+        private static void Main()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            arr = new int[n];
+            GenerateVariationsWithRepetitions(0, n);
+        }
+
+        private static void GenerateVariationsWithRepetitions(int index, int n)
+        {
+            if (index >= n)
+            {
+                PrintVariations();
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    arr[index] = i;
+                    GenerateVariationsWithRepetitions(index + 1, n);
+                }
+            }
+        }
+
+        private static void PrintVariations()
+        {
+            Console.WriteLine("{0}", string.Join(" ", arr));
+        }
+    }
+}
