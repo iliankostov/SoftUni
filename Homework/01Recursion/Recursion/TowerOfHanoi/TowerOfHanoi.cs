a27e012c748f734f8fe318f8a6286506bbdddf5d
iliankostov <ilian.kostov@gmail.com>
2015-09-27 17:54:51 +0300

    Solve homework 01 problem 01 Tower of Hanoi

---
+++ Homework/01Recursion/Recursion/TowerOfHanoi/TowerOfHanoi.cs
@@ -0,0 +1,54 @@
+﻿namespace TowerOfHanoi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class TowerOfHanoi
+    {
+        private static Stack<int> destination = new Stack<int>();
+
+        private static Stack<int> source;
+
+        private static Stack<int> spare = new Stack<int>();
+
+        private static int stepsTaken;
+
+        private static void Main()
+        {
+            Console.Write("Enter number of disks: ");
+            int numberOfDisks = int.Parse(Console.ReadLine());
+            source = new Stack<int>(Enumerable.Range(1, numberOfDisks).Reverse());
+            PrintRods();
+            MoveDisks(source.Count, source, destination, spare);
+        }
+
+        private static void MoveDisks(int bottomDisk, Stack<int> source, Stack<int> destination, Stack<int> spare)
+        {
+            if (bottomDisk == 1)
+            {
+                stepsTaken++;
+                destination.Push(source.Pop());
+                Console.WriteLine($"Step #{stepsTaken}: Moved disk {bottomDisk}");
+                PrintRods();
+            }
+            else
+            {
+                MoveDisks(bottomDisk - 1, source, spare, destination);
+                stepsTaken++;
+                destination.Push(source.Pop());
+                Console.WriteLine($"Step #{stepsTaken}: Moved disk {bottomDisk}");
+                PrintRods();
+                MoveDisks(bottomDisk - 1, spare, destination, source);
+            }
+        }
+
+        private static void PrintRods()
+        {
+            Console.WriteLine("Source: {0}", string.Join(", ", source.Reverse()));
+            Console.WriteLine("Destination: {0}", string.Join(", ", destination.Reverse()));
+            Console.WriteLine("Spare: {0}", string.Join(", ", spare.Reverse()));
+            Console.WriteLine();
+        }
+    }
+}
