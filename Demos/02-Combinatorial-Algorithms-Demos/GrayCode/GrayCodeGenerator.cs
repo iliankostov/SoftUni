b0eee21339765a3af6424297c3214375b90afcfe
iliankostov <ilian.kostov@gmail.com>
2015-09-27 18:01:52 +0300

    Review demo 02 Combinatorics

---
+++ Demos/02-Combinatorial-Algorithms-Demos/GrayCode/GrayCodeGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+internal class GrayCodeGenerator
+{
+    private const int n = 4;
+
+    private static int[] arr = new int[n];
+
+    private static void BackwardGray(int k)
+    {
+        if (k < 0)
+        {
+            Print();
+        }
+        else
+        {
+            arr[k] = 1;
+            ForwardGray(k - 1);
+            arr[k] = 0;
+            BackwardGray(k - 1);
+        }
+    }
+
+    private static void ForwardGray(int k)
+    {
+        if (k < 0)
+        {
+            Print();
+        }
+        else
+        {
+            arr[k] = 0;
+            ForwardGray(k - 1);
+            arr[k] = 1;
+            BackwardGray(k - 1);
+        }
+    }
+
+    private static void Main()
+    {
+        ForwardGray(n - 1);
+    }
+
+    private static void Print()
+    {
+        Console.WriteLine("(" + string.Join(", ", arr) + ")");
+    }
+}
---
+++ Demos/02-Combinatorial-Algorithms-Demos/Variations-No-Repetitions/VariationsGeneratorNoReps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+internal class VariationsGeneratorNoReps
+{
+    private const int k = 2;
+
+    private const int n = 10;
+
+    private static string[] objects = new string[n]
+                                          {
+                                              "banana", "apple", "orange", "strawberry", "raspberry", "apricot", "cherry",
+                                              "lemon", "grapes", "melon"
+                                          };
+
+    private static int[] arr = new int[k];
+
+    private static bool[] used = new bool[n];
+
+    private static void GenerateVariationsNoRepetitions(int index)
+    {
+        if (index >= k)
+        {
+            PrintVariations();
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    arr[index] = i;
+                    GenerateVariationsNoRepetitions(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+
+    private static void Main()
+    {
+        GenerateVariationsNoRepetitions(0);
+    }
+
+    private static void PrintVariations()
+    {
+        Console.WriteLine("({0}) --> ({1})", string.Join(", ", arr), string.Join(", ", arr.Select(i => objects[i])));
+    }
+}
