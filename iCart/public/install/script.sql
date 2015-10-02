00ad7512f128f68f5a5f2dffe6aecc0cb429e27b
iliankostov <ilian.kostov@gmail.com>
2015-10-02 11:16:03 +0300

    Create base database script

--- iCart/public/install/script.sql
+++ iCart/public/install/script.sql
@@ -6,13 +6,63 @@ CREATE DATABASE `i_cart`
 
 USE `i_cart`;
 
+CREATE TABLE `roles` (
+  id   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+INSERT INTO roles (name)
+VALUES ('admin'), ('editor'), ('user');
+
 CREATE TABLE `users` (
   id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255),
-  email    VARCHAR(255),
   password VARCHAR(255),
-  cash     DECIMAL(18, 9)
+  role_id  INT,
+  cash     DECIMAL(18, 9),
+  FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
-INSERT INTO users (username, email, password, cash)
-VALUES ('admin', 'admin@example.com', '$2y$10$ilwmxJFaAnSJ03DzgUD4u.jUQWzSN7mEsijwJn7GF6Z2dpAtiWXYO', 1000);
+INSERT INTO users (username, password, role_id, cash)
+VALUES ('admin', '$2y$10$ilwmxJFaAnSJ03DzgUD4u.jUQWzSN7mEsijwJn7GF6Z2dpAtiWXYO', 1, 1000000);
+
+CREATE TABLE `categories` (
+  id   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+INSERT INTO categories (name)
+VALUES ('laptops'), ('notebooks'), ('tablets'), ('smartphones'), ('accessoaries');
+
+CREATE TABLE `products` (
+  id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name        VARCHAR(255),
+  model       VARCHAR(255),
+  price       DECIMAL(18, 9),
+  quantity   INT,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE `users_products` (
+  id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id    INT,
+  product_id INT,
+  if_cart    BIT,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE `available_products` (
+  id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id    INT,
+  product_id INT,
+  discount   INT,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products (id)
+    ON DELETE CASCADE
+);
