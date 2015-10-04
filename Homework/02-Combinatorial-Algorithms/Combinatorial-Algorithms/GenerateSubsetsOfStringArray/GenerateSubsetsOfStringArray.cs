ef31236640b2ed56f65244c57effeba479dbe113
iliankostov <ilian.kostov@gmail.com>
2015-10-04 18:50:48 +0300

    Solve homework 02 problem 04 Generate Subsets of String Array

---
+++ Homework/02-Combinatorial-Algorithms/Combinatorial-Algorithms/GenerateSubsetsOfStringArray/GenerateSubsetsOfStringArray.cs
@@ -0,0 +1,37 @@
+﻿namespace GenerateSubsetsOfStringArray
+{
+    using System;
+    using System.Linq;
+
+    internal class GenerateSubsetsOfStringArray
+    {
+        private const int K = 2;
+
+        private static readonly string[] Objects = { "test", "rock", "fun" };
+
+        private static readonly int N = Objects.Length;
+
+        private static readonly int[] Array = new int[K];
+
+        private static void GenerateSubsets(int index, int start)
+        {
+            if (index >= K)
+            {
+                Console.WriteLine("({0})", string.Join(", ", Array.Select(i => Objects[i])));
+            }
+            else
+            {
+                for (int i = start; i < N; i++)
+                {
+                    Array[index] = i;
+                    GenerateSubsets(index + 1, i + 1);
+                }
+            }
+        }
+
+        private static void Main()
+        {
+            GenerateSubsets(0, 0);
+        }
+    }
+}
