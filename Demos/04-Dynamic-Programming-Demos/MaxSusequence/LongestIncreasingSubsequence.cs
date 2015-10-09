f301ebda2e48a8d49bca459db1b683466e21011e
iliankostov <ilian.kostov@gmail.com>
2015-10-09 21:05:47 +0300

    Review demo 04 Dynamic Programming

---
+++ Demos/04-Dynamic-Programming-Demos/MaxSusequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class LongestIncreasingSubsequence
+{
+    private const int NO_PREVIOUS = -1;
+
+    private static int CalculateLongestIncreasingSubsequence(int[] seq, int[] len, int[] prev)
+    {
+        int bestLen = 0;
+        int bestIndex = 0;
+        for (int x = 0; x < seq.Length; x++)
+        {
+            len[x] = 1;
+            prev[x] = NO_PREVIOUS;
+            for (int i = 0; i <= x - 1; i++)
+            {
+                if (seq[i] < seq[x] && 1 + len[i] > len[x])
+                {
+                    len[x] = 1 + len[i];
+                    prev[x] = i;
+                    if (len[x] > bestLen)
+                    {
+                        bestLen = len[x];
+                        bestIndex = x;
+                    }
+                }
+            }
+        }
+        return bestIndex;
+    }
+
+    private static void Main()
+    {
+        int[] seq = { 3, 4, 8, 1, 2, 4, 32, 6, 2, 5, 33, 4, 38, 22 };
+        int[] len = new int[seq.Length];
+        int[] prev = new int[seq.Length];
+
+        int bestIndex = CalculateLongestIncreasingSubsequence(seq, len, prev);
+
+        Console.WriteLine("seq[]  = " + String.Join(", ", seq));
+        Console.WriteLine("len[]  = " + String.Join(", ", len));
+        Console.WriteLine("prev[] = " + String.Join(", ", prev));
+
+        PrintLongestIncreasingSubsequence(seq, prev, bestIndex);
+    }
+
+    private static void PrintLongestIncreasingSubsequence(int[] seq, int[] prev, int index)
+    {
+        List<int> lis = new List<int>();
+        while (index != NO_PREVIOUS)
+        {
+            lis.Add(seq[index]);
+            index = prev[index];
+        }
+        lis.Reverse();
+        Console.WriteLine("subsequence = [{0}]", string.Join(", ", lis));
+    }
+}
---
+++ Demos/04-Dynamic-Programming-Demos/SubSetSumsMatrix/SubSetSumsMatrix.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class SubSetSums
+{
+    private static int[] nums = { 5, 5, 15, 20, 1 };
+
+    private static int sum = 26;
+
+    private static bool[,] isCalculated = new bool[nums.Length, sum + 1];
+
+    private static bool[,] possibleSum = new bool[nums.Length, sum + 1];
+
+    private static bool CalcPossibleSums(int i, int sum)
+    {
+        if (sum < 0 || i < 0)
+        {
+            return false;
+        }
+
+        if (!isCalculated[i, sum])
+        {
+            possibleSum[i, sum] = (nums[i] == sum) || CalcPossibleSums(i - 1, sum)
+                                  || CalcPossibleSums(i - 1, sum - nums[i]);
+            isCalculated[i, sum] = true;
+        }
+
+        return possibleSum[i, sum];
+    }
+
+    private static void Main()
+    {
+        bool possibleSum = CalcPossibleSums(nums.Length - 1, sum);
+        if (possibleSum)
+        {
+            PrintSubset(nums.Length - 1, sum);
+        }
+        else
+        {
+            Console.WriteLine("Not possible!");
+        }
+    }
+
+    private static void PrintSubset(int i, int sum)
+    {
+        Console.Write(sum + " = ");
+        List<int> nums = new List<int>();
+        while (true)
+        {
+            if (SubSetSums.nums[i] == sum)
+            {
+                // Take the last sum
+                nums.Add(SubSetSums.nums[i]);
+                break;
+            }
+            if (CalcPossibleSums(i - 1, sum - SubSetSums.nums[i]))
+            {
+                // Take arr[i]
+                nums.Add(SubSetSums.nums[i]);
+                sum = sum - SubSetSums.nums[i];
+                i = i - 1;
+            }
+            else if (CalcPossibleSums(i - 1, sum))
+            {
+                // Skip arr[i]
+                i = i - 1;
+            }
+        }
+        Console.WriteLine(string.Join(" + ", nums));
+    }
+}
