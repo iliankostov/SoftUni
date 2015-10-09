11007bdec1d986398b82357b7180c69ffcc40565
iliankostov <ilian.kostov@gmail.com>
2015-10-09 20:59:15 +0300

    Review Combinatorial Algorithms

---
+++ Practice/02-Combinatorial-Algorithms-Lab-Exercises/Combinatorial-Algorithms-Lab/01GenerateVariationsWithRepetition/VariationsWithRepetition.cs
@@ -0,0 +1,39 @@
+﻿namespace GenerateVariationsWithRepetition
+{
+    using System;
+
+    internal class VariationsWithRepetition
+    {
+        private static void GenerateVarWithRepetition(int[] arr, int sizeOfSet, int index = 0)
+        {
+            if (index >= arr.Length)
+            {
+                Print(arr);
+            }
+            else
+            {
+                for (int i = 1; i <= sizeOfSet; i++)
+                {
+                    arr[index] = i;
+                    GenerateVarWithRepetition(arr, sizeOfSet, index + 1);
+                }
+            }
+        }
+
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int k = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[k];
+
+            GenerateVarWithRepetition(arr, n);
+        }
+
+        private static void Print(int[] arr)
+        {
+            Console.WriteLine("({0})", string.Join(", ", arr));
+        }
+    }
+}
