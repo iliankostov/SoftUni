89976b27e0f876ce00674d2351aabd3b522879b5
iliankostov <ilian.kostov@gmail.com>
2015-10-10 19:09:53 +0300

    Solve exam 09 July 2015 problem Array Slider

---
+++ Practice/Exam09Jul2015/ArraySlider/ArraySlider.cs
@@ -0,0 +1,76 @@
+﻿namespace ArraySlider
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    internal class ArraySlider
+    {
+        private static void Main()
+        {
+            var array =
+                Console.ReadLine()
+                    .Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(BigInteger.Parse)
+                    .ToArray();
+
+            string line = Console.ReadLine();
+            int currentIndex = 0;
+
+            while (line != "stop")
+            {
+                var lineArgs = line.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                int offset = int.Parse(lineArgs[0]) % array.Length;
+                string operation = lineArgs[1];
+                BigInteger operand = BigInteger.Parse(lineArgs[2]);
+
+                if (offset < 0)
+                {
+                    currentIndex = (currentIndex + offset + array.Length) % array.Length;
+                }
+                else
+                {
+                    currentIndex = (currentIndex + offset) % array.Length;
+                }
+
+                ProcessCommand(array, currentIndex, operation, operand);
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine("[{0}]", string.Join(", ", array));
+        }
+
+        private static void ProcessCommand(BigInteger[] array, int currentIndex, string operation, BigInteger operand)
+        {
+            switch (operation)
+            {
+                case "&":
+                    array[currentIndex] &= operand;
+                    break;
+                case "|":
+                    array[currentIndex] |= operand;
+                    break;
+                case "^":
+                    array[currentIndex] ^= operand;
+                    break;
+                case "+":
+                    array[currentIndex] += operand;
+                    break;
+                case "-":
+                    array[currentIndex] -= operand;
+                    if (array[currentIndex] < 0)
+                    {
+                        array[currentIndex] = 0;
+                    }
+
+                    break;
+                case "*":
+                    array[currentIndex] *= operand;
+                    break;
+                case "/":
+                    array[currentIndex] /= operand;
+                    break;
+            }
+        }
+    }
+}
