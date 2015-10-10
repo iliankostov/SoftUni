9ccc1665adb6b517812154ea6929e1e9776f4df6
iliankostov <ilian.kostov@gmail.com>
2015-10-10 23:32:52 +0300

    Solve exam 31 May 2015 problem Olimpic Are Coming

---
+++ Practice/Exam31May2015/OlympicsAreComing/OlympicsAreComing.cs
@@ -0,0 +1,49 @@
+﻿namespace OlympicsAreComing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class OlympicsAreComing
+    {
+        private static void Main()
+        {
+            var line = Console.ReadLine();
+            var data = new Dictionary<string, Dictionary<string, int>>();
+
+            while (line != "report")
+            {
+                var lineArgs = line.Split('|');
+                string name = string.Join(" ", lineArgs[0].Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries));
+                string country = string.Join(
+                    " ",
+                    lineArgs[1].Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries));
+
+                if (!data.ContainsKey(country))
+                {
+                    data[country] = new Dictionary<string, int>();
+                }
+
+                if (!data[country].ContainsKey(name))
+                {
+                    data[country][name] = 0;
+                }
+
+                data[country][name]++;
+
+                line = Console.ReadLine();
+            }
+
+            var sortedData = data.OrderByDescending(d => d.Value.Sum(w => w.Value));
+
+            foreach (var country in sortedData)
+            {
+                Console.WriteLine(
+                    "{0} ({1} participants): {2} wins",
+                    country.Key,
+                    country.Value.Count,
+                    country.Value.Sum(w => w.Value));
+            }
+        }
+    }
+}
