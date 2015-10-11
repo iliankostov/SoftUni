36df8eeee1fa86ed19f9c8df330dcf7b6f3191c7
iliankostov <ilian.kostov@gmail.com>
2015-10-11 16:25:28 +0300

    My exam 11 Octomber 2015

---
+++ Practice/Exam11Oct2015/ArrayManipulator/ArrayManipulator.cs
@@ -0,0 +1,224 @@
+﻿namespace ArrayManipulator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class ArrayManipulator
+    {
+        private static List<long> collection;
+
+        private static void ExecuteCommand(string[] commandArgs)
+        {
+            string operation = commandArgs[0].Trim();
+
+            switch (operation)
+            {
+                case "exchange":
+                    ExecuteExchangeCommand(commandArgs);
+                    break;
+                case "max":
+                    ExecuteMaxCommand(commandArgs);
+
+                    break;
+                case "min":
+                    ExecuteMinCommand(commandArgs);
+
+                    break;
+                case "first":
+                    ExecuteFirstCommand(commandArgs);
+
+                    break;
+                case "last":
+                    ExecuteLastCommand(commandArgs);
+
+                    break;
+            }
+        }
+
+        private static void ExecuteExchangeCommand(string[] commandArgs)
+        {
+            long index = int.Parse(commandArgs[1].Trim());
+
+            if (index < collection.Count)
+            {
+                var queue = new Queue<long>(collection);
+                for (int i = 0; i <= index; i++)
+                {
+                    long element = queue.Dequeue();
+                    queue.Enqueue(element);
+                }
+
+                collection = new List<long>(queue);
+            }
+            else
+            {
+                Console.WriteLine("Invalid index");
+            }
+        }
+
+        private static void ExecuteFirstCommand(string[] commandArgs)
+        {
+            long count = long.Parse(commandArgs[1].Trim());
+            if (count <= collection.Count)
+            {
+                string eo = commandArgs[2].Trim();
+                var result = new List<long>();
+                if (eo == "even")
+                {
+                    foreach (var element in collection)
+                    {
+                        if (result.Count < 2 && element % 2 == 0)
+                        {
+                            result.Add(element);
+                        }
+                    }
+                }
+
+                if (eo == "odd")
+                {
+                    foreach (var element in collection)
+                    {
+                        if (result.Count < 2 && element % 2 == 1)
+                        {
+                            result.Add(element);
+                        }
+                    }
+                }
+
+                Console.WriteLine("[{0}]", string.Join(", ", result));
+            }
+            else
+            {
+                Console.WriteLine("Invalid count");
+            }
+        }
+
+        private static void ExecuteLastCommand(string[] commandArgs)
+        {
+            long count = long.Parse(commandArgs[1].Trim());
+            if (count <= collection.Count)
+            {
+                var result = new List<long>();
+                string eo = commandArgs[2].Trim();
+                if (eo == "even")
+                {
+                    result.AddRange(collection.Where(element => element % 2 == 0));
+                }
+
+                if (eo == "odd")
+                {
+                    result.AddRange(collection.Where(element => element % 2 == 1));
+                }
+
+                int skip = (int)(result.Count - count);
+
+                Console.WriteLine("[{0}]", string.Join(", ", result.Skip(skip)));
+            }
+            else
+            {
+                Console.WriteLine("Invalid count");
+            }
+        }
+
+        private static void ExecuteMaxCommand(string[] commandArgs)
+        {
+            var list = new List<long>(collection);
+            long maxElement = long.MinValue;
+            long? index = null;
+
+            if (commandArgs[1].Trim() == "even")
+            {
+                foreach (var element in list)
+                {
+                    if (element % 2 == 0 && element > maxElement)
+                    {
+                        maxElement = element;
+                        index = list.IndexOf(maxElement);
+                    }
+                }
+            }
+
+            if (commandArgs[1].Trim() == "odd")
+            {
+                foreach (var element in list)
+                {
+                    if (element % 2 == 1 && element > maxElement)
+                    {
+                        maxElement = element;
+                        index = list.IndexOf(maxElement);
+                    }
+                }
+            }
+
+            if (index != null)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+
+        private static void ExecuteMinCommand(string[] commandArgs)
+        {
+            var list = new List<long>(collection);
+            long minElement = long.MaxValue;
+            long? index = null;
+
+            if (commandArgs[1].Trim() == "even")
+            {
+                foreach (var element in list)
+                {
+                    if (element % 2 == 0 && element < minElement)
+                    {
+                        minElement = element;
+                        index = list.IndexOf(minElement);
+                    }
+                }
+            }
+
+            if (commandArgs[1].Trim() == "odd")
+            {
+                foreach (var element in list)
+                {
+                    if (element % 2 == 1 && element < minElement)
+                    {
+                        minElement = element;
+                        index = list.IndexOf(minElement);
+                    }
+                }
+            }
+
+            if (index != null)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+
+        private static void Main()
+        {
+            collection =
+                Console.ReadLine()
+                    .Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(long.Parse)
+                    .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                ExecuteCommand(command.Split(new char[] {}, StringSplitOptions.RemoveEmptyEntries));
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("[{0}]", string.Join(", ", collection));
+        }
+    }
+}
---
+++ Practice/Exam11Oct2015/RadioactiveMutantVampireBunnies/RadioactiveMutantVampireBunnies.cs
@@ -0,0 +1,218 @@
+﻿namespace RadioactiveMutantVampireBunnies
+{
+    using System;
+
+    internal class RadioactiveMutantVampireBunnies
+    {
+        private static int cols;
+
+        private static char[,] matrix;
+
+        private static int rows;
+
+        private static string ExpandBunies()
+        {
+            string output = null;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] == 'B')
+                    {
+                        if (row != 0)
+                        {
+                            if (matrix[row - 1, col] == 'P')
+                            {
+                                output = string.Format("dead {0} {1}", row - 1, col);
+                            }
+
+                            matrix[row - 1, col] = 'B';
+                        }
+
+                        if (row != rows - 1)
+                        {
+                            if (matrix[row + 1, col] == 'P')
+                            {
+                                output = string.Format("dead {0} {1}", row + 1, col);
+                            }
+
+                            matrix[row + 1, col] = 'B';
+                        }
+
+                        if (col != 0)
+                        {
+                            if (matrix[row, col - 1] == 'P')
+                            {
+                                output = string.Format("dead {0} {1}", row, col - 1);
+                            }
+
+                            matrix[row, col - 1] = 'B';
+                        }
+
+                        if (col != cols - 1)
+                        {
+                            if (matrix[row, col + 1] == 'P')
+                            {
+                                output = string.Format("dead {0} {1}", row, col + 1);
+                            }
+
+                            matrix[row, col + 1] = 'B';
+                        }
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        private static void FillMatrix()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                var line = Console.ReadLine();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+        }
+
+        private static void Main()
+        {
+            var dimensions = Console.ReadLine().Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+            rows = int.Parse(dimensions[0]);
+            cols = int.Parse(dimensions[1]);
+            matrix = new char[rows, cols];
+
+            FillMatrix();
+
+            var directions = Console.ReadLine();
+
+            var result = ProcessTurn(directions);
+
+            if (result != null)
+            {
+                PrintMatrix();
+                Console.WriteLine(result);
+            }
+        }
+
+        private static string MovePlayer(char direction)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] == 'P')
+                    {
+                        switch (direction)
+                        {
+                            case 'U':
+                                if (row == 0)
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("won {0} {1}", row, col);
+                                }
+
+                                if (matrix[row - 1, col] == 'B')
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("dead {0} {1}", row, col);
+                                }
+
+                                matrix[row, col] = '.';
+                                matrix[row - 1, col] = 'P';
+                                break;
+                            case 'L':
+                                if (col == 0)
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("won {0} {1}", row, col);
+                                }
+
+                                if (matrix[row, col - 1] == 'B')
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("dead {0} {1}", row, col);
+                                }
+
+                                matrix[row, col] = '.';
+                                matrix[row, col - 1] = 'P';
+                                break;
+                            case 'D':
+                                if (row == rows - 1)
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("won {0} {1}", row, col);
+                                }
+
+                                if (matrix[row + 1, col] == 'B')
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("dead {0} {1}", row, col);
+                                }
+
+                                matrix[row, col] = '.';
+                                matrix[row + 1, col] = 'P';
+                                break;
+                            case 'R':
+                                if (col == cols - 1)
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("won {0} {1}", row, col);
+                                }
+
+                                if (matrix[row, col + 1] == 'B')
+                                {
+                                    matrix[row, col] = '.';
+                                    return string.Format("dead {0} {1}", row, col);
+                                }
+
+                                matrix[row, col] = '.';
+                                matrix[row, col + 1] = 'P';
+
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static void PrintMatrix()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static string ProcessTurn(string directions)
+        {
+            for (int turn = 0; turn < directions.Length; turn++)
+            {
+                var player = MovePlayer(directions[turn]);
+                var bunies = ExpandBunies();
+
+                if (player != null)
+                {
+                    return player;
+                }
+
+                if (bunies != null)
+                {
+                    return bunies;
+                }
+            }
+
+            return null;
+        }
+    }
+}
