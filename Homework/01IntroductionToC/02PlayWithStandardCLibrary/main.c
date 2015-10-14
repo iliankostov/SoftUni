54fb4d186d91c324164a1dacd24b6776e2fc9e80
iliankostov <ilian.kostov@gmail.com>
2015-10-14 12:08:26 +0300

    Revert "Modify problem 02 with correct scan"
    This reverts commit 87440a13f51d2bac36a22b754b7ee10994210140.

--- Homework/01IntroductionToC/02PlayWithStandardCLibrary/main.c
+++ Homework/01IntroductionToC/02PlayWithStandardCLibrary/main.c
@@ -3,10 +3,9 @@
 
 int main(int argc, char** argv) {
     
-    printf("Type something up to 10 symbols: ");
     char greeting[10];
-    scanf("%9s", greeting);
-    printf("You typed %s!", greeting);
+    scanf("%s", &greeting);
+    printf("%s", greeting);
     
     return (EXIT_SUCCESS);
 }
