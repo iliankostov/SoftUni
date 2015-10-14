51d175e26c8dadfea4d4ea665c9bab28d92a61eb
iliankostov <ilian.kostov@gmail.com>
2015-10-14 09:55:36 +0300

    Solve homework 01 problems 01-07

---
+++ Homework/01IntroductionToC/04PrintYourName/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    printf("%s", "My name is Pesho.");
+    
+    return (EXIT_SUCCESS);
+}
---
+++ Homework/01IntroductionToC/05PrintNumbers/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    int one = 1;
+    int oneZeroOne = 101;
+    int oneZeroZeroOne = 1001;
+    
+    printf("%d\n%d\n%d", one, oneZeroOne, oneZeroZeroOne);
+    
+    return (EXIT_SUCCESS);
+}
