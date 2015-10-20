fa0ed90331c5e8fd53deafdd528146846f8497ea
iliankostov <ilian.kostov@gmail.com>
2015-10-20 14:16:42 +0300

    Solve homework 05 problem 01 Exchange if Greater

--- Homework/05ConditionalStatements/01ExchangeIfGreater/main.c
+++ Homework/05ConditionalStatements/01ExchangeIfGreater/main.c
@@ -1,18 +1,21 @@
-/* 
- * File:   main.c
- * Author: ilian
- *
- * Created on October 20, 2015, 2:09 PM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
 int main(int argc, char** argv) {
+    
+    double a, b, temp;
+    printf("a = ");
+    scanf("%lf", &a);
+    printf("b = ");
+    scanf("%lf", &b);
+    
+    if (a > b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    
+    printf("%.2lf %.2lf", a, b);
 
     return (EXIT_SUCCESS);
 }
-
