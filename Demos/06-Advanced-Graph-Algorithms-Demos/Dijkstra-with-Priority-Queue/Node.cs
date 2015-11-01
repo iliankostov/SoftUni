a75962278b865c8c01c56984c4a6a0acd5a99014
iliankostov <ilian.kostov@gmail.com>
2015-11-01 11:23:16 +0200

    Review Advanced Graph Algorithms Demos

---
+++ Demos/06-Advanced-Graph-Algorithms-Demos/Dijkstra-with-Priority-Queue/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace DijkstraWithPriorityQueue
+{
+    using System;
+
+    public class Node : IComparable
+    {
+        public Node(int id)
+        {
+            this.Id = id;
+        }
+
+        public int Id { get; private set; }
+
+        public double Distance { get; set; }
+
+        public Node PreviousNode { get; set; }
+
+        public int CompareTo(object otherNode)
+        {
+            return this.Distance.CompareTo((otherNode as Node).Distance);
+        }
+    }
+}
---
+++ Demos/06-Advanced-Graph-Algorithms-Demos/Strongly-Connected-Components/StronglyConnectedComponents.cs
@@ -0,0 +1,119 @@
+﻿namespace StronglyConnectedComponents
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class StronglyConnectedComponents
+    {
+        // Kosaraju–Sharir algorithm for finding the strongly-connected
+        // components in directed graph, implemented with DFS + reverse DFS.
+        // Running time: O(N + M)
+        // Learn more at: https://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
+
+        private static int n;
+
+        private static List<int>[] graph;
+
+        private static List<int>[] reverseGraph;
+
+        private static bool[] visited;
+
+        private static Stack<int> dfsNodesStack;
+
+        private static void BuildReverseGraph()
+        {
+            reverseGraph = new List<int>[n];
+            for (int node = 0; node < n; node++)
+            {
+                reverseGraph[node] = new List<int>();
+            }
+
+            for (int node = 0; node < n; node++)
+            {
+                foreach (var childNode in graph[node])
+                {
+                    // Add the reverse edge {childNode -> node}
+                    reverseGraph[childNode].Add(node);
+                }
+            }
+        }
+
+        private static void DFS(int node)
+        {
+            if (!visited[node])
+            {
+                visited[node] = true;
+                foreach (var childNode in graph[node])
+                {
+                    DFS(childNode);
+                }
+                dfsNodesStack.Push(node);
+            }
+        }
+
+        private static void ReverseDFS(int node)
+        {
+            if (!visited[node])
+            {
+                visited[node] = true;
+                Console.Write(" {0}", node);
+                foreach (var childNode in reverseGraph[node])
+                {
+                    ReverseDFS(childNode);
+                }
+            }
+        }
+
+        private static void FindStronglyConnectedComponents()
+        {
+            n = graph.Length;
+            BuildReverseGraph();
+
+            // Traverse the graph with DFS and push all nodes in the stack
+            // in post-order (on return from recursion)
+            visited = new bool[n];
+            dfsNodesStack = new Stack<int>();
+            for (int node = 0; node < n; node++)
+            {
+                DFS(node);
+            }
+
+            // Traverse the nodes from the stack and perform reverse DFS
+            // to find the strongly-connected components
+            visited = new bool[n];
+            while (dfsNodesStack.Count > 0)
+            {
+                var node = dfsNodesStack.Pop();
+                if (!visited[node])
+                {
+                    Console.Write("{");
+                    ReverseDFS(node);
+                    Console.WriteLine(" }");
+                }
+            }
+        }
+
+        private static void Main()
+        {
+            graph = new List<int>[]
+                {
+                    new List<int>() { 1, 11, 13 }, // children of node 0
+                    new List<int>() { 6 }, // children of node 1
+                    new List<int>() { 0 }, // children of node 2
+                    new List<int>() { 4 }, // children of node 3
+                    new List<int>() { 3, 6 }, // children of node 4
+                    new List<int>() { 13 }, // children of node 5
+                    new List<int>() { 0, 11 }, // children of node 6
+                    new List<int>() { 12 }, // children of node 7
+                    new List<int>() { 6, 11 }, // children of node 8
+                    new List<int>() { 0 }, // children of node 9
+                    new List<int>() { 4, 6, 10 }, // children of node 10
+                    new List<int>() { }, // children of node 11
+                    new List<int>() { 7 }, // children of node 12
+                    new List<int>() { 2, 9 }, // children of node 13
+                };
+
+            FindStronglyConnectedComponents();
+        }
+    }
+}
