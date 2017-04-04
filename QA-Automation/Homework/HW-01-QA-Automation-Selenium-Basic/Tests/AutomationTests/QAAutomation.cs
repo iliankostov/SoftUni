12d853b16e4cb78e52c4db23687272d837e0dd97
iliankostov <ilian.kostov@gmail.com>
2017-04-04 21:17:53 +0300

    Update libs and QAAutomaiton test

--- QA-Automation/Homework/HW-01-QA-Automation-Selenium-Basic/Tests/AutomationTests/QAAutomation.cs
+++ QA-Automation/Homework/HW-01-QA-Automation-Selenium-Basic/Tests/AutomationTests/QAAutomation.cs
@@ -24,8 +24,16 @@
             var educationsLink = driver.FindElement(By.XPath("//*[@id=\"header-nav\"]/div[1]/ul/li[2]/a"));
             educationsLink.Click();
 
-            var courseLink = driver.FindElement(By.XPath("//*[@id=\"header-nav\"]/div[1]/ul/li[2]/div/div/div/div[2]/div[2]/ul/li[6]/a"));
-            courseLink.Click();
+            var activeCourses = driver.FindElements(By.XPath("//*[@id=\"header-nav\"]/div[1]/ul/li[2]/div/div/div/div[2]/div[2]/ul/li"));
+
+            foreach (var activeCourse in activeCourses)
+            {
+                if (activeCourse.Text == "QA Automation - март 2017")
+                {
+                    activeCourse.Click();
+                    break;
+                }
+            }
 
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             var headings = wait.Until((w) =>
