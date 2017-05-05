208028ddbda3d0bc51f87b11d402ae632a4c7874
Ilian Kostov <ilian.kostov@gmail.com>
2017-05-05 16:03:05 +0300

    Extract selectors

--- QA-Automation/Homework/HW-03-QA-Automation-Selenium-WebDriver-Advanced/Tests/AutomationTests/Pages/SelectablePage/SelectablePageConstants.cs
+++ QA-Automation/Homework/HW-03-QA-Automation-Selenium-WebDriver-Advanced/Tests/AutomationTests/Pages/SelectablePage/SelectablePageConstants.cs
@@ -4,9 +4,15 @@
     {
         //// Common
         internal const string SELECTABLE_URL = "selectable/";
-        internal const int WAIT_SECCONDS = 20;     
+        internal const int WAIT_SECCONDS = 20;
 
         //// Selectors
-        
+        internal const string DEFAULT_TAB_SELECTOR = "ui-id-1";
+        internal const string DISPLAY_AS_GRID_TAB_SELECTOR = "ui-id-2";
+        internal const string SERIALIZE_TAB_SELECTOR = "ui-id-3";
+        internal const string SELECTABLE_LIST_SELECTOR = "selectable";
+        internal const string SELECTABLE_GRID_SELECTOR = "selectable_grid";
+        internal const string SELECTABLE_SERIALIZIZE_SELECTOR = "selectable-serialize";
+        internal const string SELECT_RESULT_SELECTOR = "select-result";
     }
 }
--- QA-Automation/Homework/HW-03-QA-Automation-Selenium-WebDriver-Advanced/Tests/AutomationTests/Pages/SortablePage/SortablePageConstants.cs
+++ QA-Automation/Homework/HW-03-QA-Automation-Selenium-WebDriver-Advanced/Tests/AutomationTests/Pages/SortablePage/SortablePageConstants.cs
@@ -4,9 +4,15 @@
     {
         //// Common
         internal const string SORTABLE_URL = "sortable/";
-        internal const int WAIT_SECCONDS = 20;     
+        internal const int WAIT_SECCONDS = 20;
 
         //// Selectors
-        
+        internal const string DEFAULT_TAB_SELECTOR = "ui-id-1";
+        internal const string CONNECT_LISTS_TAB_SELECTOR = "ui-id-2";
+        internal const string DISPLAY_AS_GRID_TAB_SELECTOR = "ui-id-3";
+        internal const string SORTABLE_LIST_SELECTOR = "sortable";
+        internal const string CONNECT_LIST_ONE_SELECTOR = "sortable1";
+        internal const string CONNECT_LIST_TWO_SELECTOR = "sortable2";
+        internal const string SORTABLE_GRID_SELECTOR = "sortable_grid";
     }
 }
